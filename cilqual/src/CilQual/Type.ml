b7cd38a6ec0bfd6a6122eb4d2f30a2cdd32bc5ae
Khoo Yit Phang <khooyp@cs.umd.edu>
2009-04-12 00:43:18 +0000

    Qualifier constant edges should include context. Wonder why I thought otherwise.

--- cilqual/src/CilQual/Type.ml
+++ cilqual/src/CilQual/Type.ml
@@ -93,7 +93,7 @@ module InterpreterT (C : Config.InterpreterMonad) = struct
         in
         let ts = Cil.typeSig t in
         qt <-- create (embed_ts ts);
-        inContext (fun _ -> emptyContext) (annot_qt qt ts) (* annotate qualifiers ignoring context *)
+        annot_qt qt ts
 
     let embed_lval t = perform
         if CilType.is_or_points_to_function t
