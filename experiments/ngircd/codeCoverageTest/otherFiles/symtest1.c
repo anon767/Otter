0d3253753fce8e8fe257b6491033b5e7e62e5ace
Kin-Keung Ma <kkma@cs.umd.edu>
2009-04-15 22:48:44 +0000

    symtest1 is now completed.

--- experiments/ngircd/codeCoverageTest/otherFiles/symtest1.c
+++ experiments/ngircd/codeCoverageTest/otherFiles/symtest1.c
@@ -218,6 +218,7 @@ void symtest1(){
 
 	// setup client(s)
 	int client_fd1 = socket(0,0,0);
+	int client_fd2 = socket(0,0,0);
 
 	t = 0;
 		
@@ -225,7 +226,19 @@ void symtest1(){
 	event_recv(client_fd1,"NICK martin\r\n",t++);
 	event_recv(client_fd1,"USER kkma x x :kin keung ma\r\n",t++);
 	event_send(client_fd1,t++);
+	event_recv(client_fd2,"NICK jeff\r\n",t++);
+	event_recv(client_fd2,"USER jfoster x x :jeff foster\r\n",t++);
+	event_send(client_fd2,t++);
 	event_recv(client_fd1,"JOIN #channel\r\n",t++);
+	event_recv(client_fd2,"JOIN #channel\r\n",t++);
+	event_recv(client_fd1,"PRIVMSG #channel :hello\r\n",t++);
+	event_recv(client_fd1,"PRIVMSG jeff :will I have to be TA next semester?\r\n",t++);
+	event_recv(client_fd2,"PART #channel\r\n",t++);
+	event_recv(client_fd1,"NAMES\r\n",t++);
+	event_recv(client_fd1,"LIST\r\n",t++);
+	event_recv(client_fd2,"WHOIS martin\r\n",t++);
+	event_recv(client_fd2,"QUIT\r\n",t++);
+	event_recv(client_fd1,"QUIT\r\n",t++);
 	event_end(t++);
 }
 
