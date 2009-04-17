2f2399e4dd000fb4e9cb646935cd9debc3e519f4
Elnatan Reisner <elnatan@cs.umd.edu>
2009-04-17 17:58:06 +0000

    Fixing FILE* versus file descriptor discrepancy. Also, added some handling of 'mode' argument.

--- mockedFns/fopen.c
+++ mockedFns/fopen.c
@@ -1,7 +1,38 @@
+#include "iosim.h"
 #include <stdio.h>
 #include <fcntl.h>
+#include <errno.h>
 
 FILE * fopen (const char * filename, const char * mode)
 {
-  return open(filename, O_RDONLY, "r");
+	int openFlags, plus = 0;
+	if (!(mode && mode[0])) { // If mode is null or ""
+		errno = EINVAL;
+		return NULL;
+	}
+
+	if (mode[1] == '+' || (mode[1] == 'b' && mode[2] == '+')) {
+		plus = 1;
+	}
+
+	switch (mode[0]) {
+	case 'r':
+		openFlags = plus ? O_RDWR : O_RDONLY;
+		break;
+	case 'w':
+		openFlags = O_CREAT | O_TRUNC | (plus ? O_RDWR : O_WRONLY);
+		break;
+	case 'a':
+		openFlags = O_CREAT | O_APPEND | (plus ? O_RDWR : O_WRONLY);
+		break;
+	default:
+		errno = EINVAL;
+		return NULL;
+	}
+
+	int fd = open(filename, openFlags, 0666); // 0666 is stated in the spec
+	if (fd == -1) {
+		return NULL;
+	}
+	return IOSIM_fd[fd];
 }
