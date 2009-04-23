dc2f7fdf23c6af03431178ca8a52380e95715e4e
Kin-Keung Ma <kkma@cs.umd.edu>
2009-04-23 17:08:09 +0000

    added a symbolic link for the test script directory.

---
+++ experiments/ngircd/codeCoverageTest/gentest.rb
@@ -0,0 +1,91 @@
+#!/usr/bin/ruby
+
+def make_testcontents(name)
+  s = <<END
+STATSFILE=../ngircd.stats
+
+TOPDIR=../../../..
+LIBCDIR=$TOPDIR/libc
+MOCKEDDIR=../mockedFns
+
+cd $1
+
+$TOPDIR/cil/bin/cilly \\
+	--merge \\
+	--useLogicalOperators \\
+	-D_FILE_OFFSET_BITS=64 \\
+	-U __OPTIMIZE__ \\
+	-I $MOCKEDDIR \\
+	--doexecute \\
+	--printLittle \\
+	--printNoEscapedString \\
+	--covStats=$STATSFILE \\
+	--lineCov \\
+	../otherFiles/symtest_driver.c \\
+	../otherFiles/#{name}.c
+END
+end
+
+def make_testfile(name)
+  file = "tests/#{name}.test"
+  File.open(file, "w") { |f|
+    f.puts(make_testcontents(name))
+  }
+  File.chmod(0755,file)
+end
+
+tests = [
+	"rfc1459_4.1.1",
+	"rfc1459_4.1.2-a",
+	"rfc1459_4.1.2-b",
+	"rfc1459_4.1.2",
+	"rfc1459_4.1.3-a",
+	"rfc1459_4.1.3",
+	"rfc1459_4.1.4",
+	"rfc1459_4.1.5",
+	"rfc1459_4.1.6",
+	"rfc1459_4.1.7",
+	"rfc1459_4.2.1-a",
+	"rfc1459_4.2.1",
+	"rfc1459_4.2.2-a",
+	"rfc1459_4.2.2",
+	"rfc1459_4.2.3.1",
+	"rfc1459_4.2.3.2",
+	"rfc1459_4.2.4",
+	"rfc1459_4.2.5",
+	"rfc1459_4.2.6",
+	"rfc1459_4.2.7",
+	"rfc1459_4.2.8",
+	"rfc1459_4.3.1",
+	"rfc1459_4.3.2",
+	"rfc1459_4.3.3",
+	"rfc1459_4.3.4",
+	"rfc1459_4.3.5",
+	"rfc1459_4.3.6",
+	"rfc1459_4.3.7",
+	"rfc1459_4.3.8",
+	"rfc1459_4.4.1-a",
+	"rfc1459_4.4.1",
+	"rfc1459_4.4.2-a",
+	"rfc1459_4.4.2",
+	"rfc1459_4.5.1",
+	"rfc1459_4.5.2",
+	"rfc1459_4.5.3",
+	"rfc1459_4.6.1",
+	"rfc1459_4.6.2",
+	"rfc1459_4.6.3",
+	"rfc1459_5.1",
+	"rfc1459_5.2",
+	"rfc1459_5.3",
+	"rfc1459_5.5",
+	"rfc1459_5.7",
+	"rfc1459_5.8",
+	"rfc2812_3.4.2",
+	"rfc2813_4.1.2",
+	"rfc2813_4.2.2"
+]
+
+tests.each { |name|
+  puts "Generate #{name}.test"
+  make_testfile(name)
+}
---
+++ experiments/ngircd/codeCoverageTest/mockedFns/poll.c
@@ -0,0 +1,95 @@
+#include <poll.h>
+#include <string.h>
+#include "iosim.h"
+
+int poll_endtime;
+
+int poll(struct pollfd fds[], nfds_t nfds, int timeout){
+	static int time = 0;
+	int i;
+	int c = 0;
+
+	if(time>=poll_endtime){
+		exit(1010);
+	}
+	for(i=3;i<nfds;i++){ // fd 0,1,2 are stdio
+		int flag = 0;
+		// If the value of fd is less than 0, events shall be ignored, 
+		// and revents shall be set to 0 in that entry on return from poll().
+		
+		fds[i].revents = 0;
+
+		if(fds[i].fd<0){
+			continue;
+		}
+
+		IOSIM_fd[fds[i].fd]->sym_file->stat.st_size 
+			= IOSIM_fd[fds[i].fd]->sym_file->size[time];
+		IOSIM_fd[fds[i].fd]->sym_fileout->stat.st_size 
+			= IOSIM_fd[fds[i].fd]->sym_fileout->size[time];
+
+
+		if(IOSIM_fd[fds[i].fd]->fd_type==IOSIM_FD_SSOCK){
+			sym_file_stream_t* ssock = IOSIM_fd[fds[i].fd];
+			if(ssock->offset < ssock->sym_file->stat.st_size){
+				//__COMMENT("ssock->offset");
+				//__EVAL(ssock->offset);
+				//__COMMENT("ssock->sym_file->stat.st_size");
+				//__EVAL(ssock->sym_file->stat.st_size);
+				fds[i].revents |= POLLPRI;
+				flag = 1;
+			}
+		}
+		else{
+			// readable?
+			// if fds[i].fd has sth in the stream, set fds[i].revents |= POLLIN
+			if(!IOSIM_eof(fds[i].fd)){
+				fds[i].revents |= POLLIN;
+				flag = 1;
+			}
+			// writable?
+			// we assume every socket is writable. 
+			// if fds[i].events | POLLOUT  (i.e., has sth to write), set fds[i].revents |= POLLOUT
+			if(fds[i].events | POLLOUT){
+				fds[i].revents |= POLLOUT;
+				flag = 1;
+			}
+			// __EVAL(fds[i].fd);
+			// __EVAL(fds[i].events);
+			// __EVAL(fds[i].revents);
+		}
+		c += flag;
+	}
+
+	time++;
+	return c;
+}
+
+int event_recv(int fd,char* s,int t){
+	sym_file_t* file = IOSIM_fd[fd]->sym_file;
+	strcat(file->contents,s);
+	file->size[t] = strlen(file->contents);
+	return 0;
+}
+
+int event_send(int fd,int t){
+	sym_file_t* out = IOSIM_fd[fd]->sym_fileout;
+	out->size[t] = 1;
+	return 0;
+}
+
+extern int listen_queue[IOSIM_MAX_EVENTS];
+extern int listen_queue_last;
+extern int listen_queue_size[IOSIM_MAX_EVENTS];
+
+int event_accept(int fd,int t){
+	listen_queue[listen_queue_last++] = fd;
+	listen_queue_size[t] = listen_queue_last;
+	return 0;
+}
+
+int event_end(int t){
+
+	poll_endtime = t;
+	return 0;
+}
