ab33f3ff7e1361d6253f6d1d2cb897d00e3a382c
Kin-Keung Ma <kkma@cs.umd.edu>
2009-04-24 18:19:14 +0000

    use slow start to minimize comparison of symbolic values

--- libc/memset.c
+++ libc/memset.c
@@ -11,10 +11,19 @@ void *memset(void *dst, int c, size_t n)
 #ifdef __FAVORITE_SE__
 	// this is better to the SE since if n is symbolic (over a "small" set of integers),
 	// keep doing n-- will blow up the symbolic expression
-	int i;
-	for(i=0;i<n;i++){
-		q[i] = c;
+	
+	// "slow start" - to minimize the no. of times of comparing symbolic values
+	
+	int i=0,nn;
+
+	if(n<=0) return dst;
+
+	for(nn=1;nn<=n;nn*=2){
+		while(i<nn) // both i and nn are concrete!
+			q[i++] = c;
 	}
+	memset(dst+i,c,n-nn/2);
+
 #else
 	while (n--) {
 		*q++ = c;
@@ -22,3 +31,4 @@ void *memset(void *dst, int c, size_t n)
 #endif
 	return dst;
 }
+
