164fa4cf0cf49654f7c971bbc0260f7b44407a40
Kin-Keung Ma <kkma@cs.umd.edu>
2009-05-11 21:15:15 +0000

    calculate size of configuration space given the spec file

---
+++ experiments/configSpace.py
@@ -0,0 +1,23 @@
+import sys,os,re
+
+if len(sys.argv) == 1:
+	print '''Usage: python configSpace.py spec.txt
+'''
+	sys.exit(0)
+
+file = open (sys.argv[1])
+line = file.readline()
+while not line.startswith("aggr"):
+	line = file.readline()
+
+result = 1
+
+line = file.readline()
+while not line.startswith("}"):
+	#print line.split()
+	n = (len(line.split())-1)
+	#print n
+	result *= n
+	line = file.readline()
+
+print result
