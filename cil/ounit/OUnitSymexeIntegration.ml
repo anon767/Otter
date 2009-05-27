1574c9d14b5552dc324b6a2a4b885884310a596e
Khoo Yit Phang <khooyp@cs.umd.edu>
2009-05-27 01:32:27 +0000

    r7847 broke a whole bunch of tests. This makes it visible in OUnitSymexeIntegration.

--- cil/ounit/OUnitSymexeIntegration.ml
+++ cil/ounit/OUnitSymexeIntegration.ml
@@ -15,8 +15,22 @@ let test_file path =
         let file = Frontc.parse (Filename.concat dir_prefix path) () in
         assert_bool "Cil parse error" (not !Errormsg.hadErrors);
 
-        (* finally run the symbolic executor *)
-        Executemain.doExecute file;
+        (* run the symbolic executor *)
+        Executemain.prepare_file file;
+        let job = Executemain.job_for_file file ["OUnitSymexeIntegration"] in
+        let results = Driver.main_loop job in
+
+        (* count jobs that were abandoned *)
+        let abandoned = List.fold_left begin fun abandoned result -> match result with
+			| Types.Abandoned (s, loc, _) -> (loc.Cil.file, loc.Cil.line, s)::abandoned
+			| _ -> abandoned
+		end [] results in
+		if abandoned <> [] then begin
+			let printer ff abandoned = ignore begin List.fold_left begin fun b (f, l, s) ->
+				Format.fprintf ff "%(%)@[%s:%d: %s@]" b f l s; "@\n"
+			end "" abandoned end in
+			assert_failure "@[<hv2>Abandoned paths:@\n%a@]" printer abandoned
+		end;
 
         (* test that no assertions failed *)
         assert_string (Executedebug.get_log ())
