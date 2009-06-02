d2897e6cb7fbb3b2856843596c3d645bd64c3cf8
Elnatan Reisner <elnatan@cs.umd.edu>
2009-06-02 04:18:49 +0000

    Guaranteed coverage calculation with some optimizations. I think it will still takes a very long time for complicated tests. Also, the getRelevantAssumptions function within Stp.consult_stp makes this run a lot faster, but that function was commented out in r7853; I've contacted Yit to what the issue was.

--- cil/src/se/calculateVarDeps.ml
+++ cil/src/se/calculateVarDeps.ml
@@ -1,6 +1,38 @@
 open Cil
 open Types
 
+let falseBytes = Bytes_Constant (Cil.CInt64 (0L,IInt,None))
+let trueBytes  = Bytes_Constant (Cil.CInt64 (1L,IInt,None))
+
+(* Do some minor simplification of logical operators. I'm not being
+	 careful to maintain a boolean context when I strip off ANDs or
+	 ORs. *)
+let rec simplifyLogicalOps bytes = match bytes with
+	| Bytes_Op(OP_LNOT,[b,typ]) ->
+			Bytes_Op(OP_LNOT, [simplifyLogicalOps b, typ])
+	| Bytes_Op(OP_LAND|OP_LOR as op, [b1,typ1 ; b2,typ2]) ->
+			begin
+				let (shortcutBytes,otherBytes) =
+					if op == OP_LAND
+					then falseBytes,trueBytes
+					else trueBytes,falseBytes
+				in
+				match simplifyLogicalOps b1 with
+					| b1' when b1' == shortcutBytes -> b1'
+					| b1' when b1' == otherBytes -> simplifyLogicalOps b2
+					| b1' ->
+							match simplifyLogicalOps b2 with
+								| b2' when b2' == shortcutBytes -> b2'
+								| b2' when b2' == otherBytes -> b1'
+								| b2' -> Bytes_Op(op, [b1',typ1 ; b2',typ2])
+			end
+	| _ ->
+			try
+				if Convert.bytes_to_bool bytes
+				then trueBytes
+				else falseBytes
+			with Failure _ -> bytes
+
 (* We need a bytesToVars in order to print out the conditions in a
 	 readable way.  I think the way we are doing things now, all of the
 	 bytesToVars should be the same, so picking an arbitrary one should be
@@ -16,7 +48,7 @@ type 'a tree =
 type varDepTree = (bytes * LineSet.t) tree
 
 (* Placeholder value for the root of the tree *)
-let rootNodeData = (Convert.lazy_int_to_bytes 1, LineSet.empty)
+let rootNodeData = (trueBytes, LineSet.empty)
 
 (* This makes a tree which is a single path. lst is represented up
 	 from the bottom of the path, with trueBytes at the root. Also, the
@@ -101,7 +133,10 @@ let valuesFile = ref ""
 (* Map variable names to possible values *)
 let varToVals = Hashtbl.create 20
 
-let numVars = ref 1
+let makeEquality bytes value =
+	(* Is intType okay here? *)
+	Bytes_Op(OP_EQ,[(bytes,intType);
+									(Convert.lazy_int_to_bytes value, intType)])
 
 (* CAUTION: Requires exponential amount of memory! *)
 let rec allPossibleValues localBytesToVars =
@@ -129,112 +164,70 @@ let rec subsetsOfSize n lst =
 			let subsetsWithoutX = subsetsOfSize n withoutX
 			and smallerSubsets = subsetsOfSize (pred n) withoutX in
 			List.rev_append
+				(List.rev_map (fun subset -> x :: subset) smallerSubsets)
 				subsetsWithoutX
-				(List.map (fun subset -> x :: subset) smallerSubsets)
 
-let makeEquality bytes value =
-	(* Is intType okay here? *)
-	Bytes_Op(OP_EQ,[(bytes,intType);
-									(Convert.lazy_int_to_bytes value, intType)])
-(*
-(* Is a pc consistent with a set of assignments? *)
-let implicationHash = Hashtbl.create 1000
+module PcAssn = Set.Make
+	(struct
+		 type t = bytes list * (bytes*int) list (* pc, assignments *)
+		 let compare ((pc1,cov1) as x:t) ((pc2,cov2) as y) = (* Type annotation to remove polymorphism *)
+			 if pc1 == pc2 (* Try physical comparison on pc1 and pc2 before structural. *)
+			 then compare cov1 cov2
+			 else compare x y
+	 end)
 
-(* Is it possible for bytes to true, given the pc? Ask STP. *)
-let mightBeTrue pc bytes =
-	try (* See if the answer is memoized *)
-		Hashtbl.find implicationHash (pc,bytes)
-	with Not_found -> (* Answer isn't memoized; ask STP *)
+(* Cache of (pc,assignment) pairs that contradict each other *)
+let contradictionCache = ref PcAssn.empty
+
+let numberOfHits = Array.make 5 0
+let numberOfUselessHits = Array.make 5 0
+let numberOfSTPCalls = Array.make 5 0
+
+let rec range a b = if a > b then [] else a :: range (succ a) b
+
+let knownContradiction pc equalities =
+	let sizesToCheck =
+		match equalities with
+				[] -> failwith "knownContradiction: empty list"
+			| [_] -> [] (* Single equalities are never already known *)
+			| [_;_] -> [1]
+			| _ -> range 1 (List.length (List.tl equalities)) (* Change this to modify the method of optimization. I'm not sure going beyond size 1, and maybe size 2, will be helpful in practice. *)
+	in
+	let existsContradictionOfSize n =
+		List.exists
+			(fun assns ->
+				 let contradictory = PcAssn.mem (pc,assns) !contradictionCache in
+				 let arr = if contradictory then numberOfHits else numberOfUselessHits in
+				 arr.(n) <- succ arr.(n);
+				 contradictory)
+			(subsetsOfSize n equalities)
+	in
+	List.exists existsContradictionOfSize sizesToCheck
+
+let isConsistentWithList pc assignments =
+	if Stats.time "knownContradiction" (fun x -> knownContradiction pc x) assignments then false
+	else
+		let theQuery =
+			match assignments with
+				| [] -> failwith "Empty list in isConsistentWithList"
+				| [var,value] -> makeEquality var value
+				| _ ->
+						Bytes_Op(OP_LAND,
+										 List.map
+											 (fun (var,value) -> (makeEquality var value, intType))
+											 assignments)
+		in
+		let size = List.length assignments in
+		numberOfSTPCalls.(size) <- succ numberOfSTPCalls.(size);
 		let result =
-			match Stp.consult_stp pc bytes Stp.FalseOrNot with
-				| Stp.False -> false (* Must be false *)
+			match Stp.consult_stp pc theQuery Stp.FalseOrNot with
+				| Stp.False ->
+						contradictionCache := PcAssn.add (pc,assignments) !contradictionCache; (* Memoize the answer *)
+						false (* Must be false *)
 				| _ -> true (* Might be true *)
 		in
-		Hashtbl.add implicationHash (pc,bytes) result; (* Memoize the answer *)
+		Output.set_mode Output.MSG_MUSTPRINT;
 		result
-
-(* This checks all subsets to see if they are inconsistent before
-	 checking the assignment itself. If subsets are often inconsistent,
-	 this should prevent calls to the SAT solver and, hence, speed
-	 things up. In some tests I tried, though, this runs much more
-	 slowly than just checking the single assignments for consistency,
-	 so I'm sticking with the other version for now. *)
-let isConsistentWithList pc assignments =
-	let rec helper size =
-		let isInconsistent =
-			List.exists
-				(fun (assns:(bytes*int) list) ->
-					 let equalities =
-						 match assns with
-							 | [] -> failwith "Empty list in isConsistentWithList"
-							 | [(varBytes,value)] -> makeEquality varBytes value
-							 | _ ->
-									 Bytes_Op(OP_LAND,
-														List.map
-															(fun (varBytes,value) -> (makeEquality varBytes value,
-																												intType))
-															assns)
-					 in
-					 not (mightBeTrue pc equalities)
-				)
-				(subsetsOfSize size assignments)
-		in
-		if isInconsistent then false
-		else if size = List.length assignments then true (* The whole thing is consistent *)
-		else helper (succ size) (* Try larger subsets *)
-	in
-	helper 1
-*)
-
-(* Memoization table recording whether a path condition is consistent
-	 with an assignment *)
-let implicationHash = Hashtbl.create 1000
-
-(* Is it possible for bytes to true, given the pc? Ask STP. *)
-let mightBeTrue pc bytes =
-	let result =
-		match Stp.consult_stp pc bytes Stp.FalseOrNot with
-			| Stp.False -> false (* Must be false *)
-			| _ -> true (* Might be true *)
-	in
-	Output.set_mode Output.MSG_MUSTPRINT;
-	result
-
-(* Is pc consistent with 'var = value'? *)
-let isConsistentWithAssignment pc (varBytes,value) =
-	try (* See if the answer is memoized *)
-		Hashtbl.find implicationHash (pc,varBytes,value)
-	with Not_found ->
-		let result = mightBeTrue pc (makeEquality varBytes value) in
-		Hashtbl.add implicationHash (pc,varBytes,value) result; (* Memoize the answer *)
-		result
-
-(* Is the pc consistent with the assignments? *)
-let isConsistentWithList pc assignments =
-	(* Check that each assignment is consistent with pc; if any one
-		 isn't consistent, then the set of assignments isn't either. *)
-	if not (List.for_all (isConsistentWithAssignment pc) assignments)
-	then false
-	else (
-		(* If each assignment is consistent, it is still possible for one
-			 to contradict another (either directly or in the context of the
-			 pc), so now we ask the SAT solver about all of the assignments
-			 at once. *)
-		match assignments with
-			| [] -> failwith "Empty list in isConsistentWithList"
-			| [_] -> true (* A single assignment cannot contradict itself *)
-			| _ ->
-					(let conjunction =
-						 Bytes_Op(OP_LAND,
-											List.map
-												(fun (varBytes,value) -> (makeEquality varBytes value,
-																								 intType))
-												assignments)
-					 in
-					 mightBeTrue pc conjunction
-					)
-	)
-
 
 (* Which of the (pc,lines) pairs have pcs which are consistent with
 	 the assignments? *)
@@ -260,9 +253,9 @@ let linesControlledBy pcsAndLines assignments =
 		Format.printf "\nUnder the condition\n%s\nthese %d lines are hit\n"
 			(String.concat "\n"
 				 (List.map
-						(fun (varBytes,value) ->
-							 Printf.sprintf "%s=%d"
-								 (To_string.humanReadableBytes !bytesToVars varBytes)
+						(fun (var,value) ->
+							 Printf.sprintf "%s=%d,"
+								 (To_string.humanReadableBytes !bytesToVars var)
 								 value)
 						assignments))
 			(LineSet.cardinal result);
@@ -270,22 +263,18 @@ let linesControlledBy pcsAndLines assignments =
 	);
 	result
 
-let findDependenciesAndUpdate (pcsAndLines,linesToExplain) localBytesToVars =
+let findDependenciesAndUpdate pcsAndLines linesToExplain localBytesToVars =
 	Format.printf "\nConsidering\n%s\n"
 		(String.concat ", " (List.map (fun (_,var) -> var.vname) localBytesToVars));
 	let linesControlledByVars =
 		bigUnion
-			(List.map
+			(List.rev_map
 				 (fun assignments ->
 						linesControlledBy pcsAndLines assignments)
 				 (allPossibleValues localBytesToVars))
 	in
-	(* Remove the lines that have been accounted for from each pair of
-		 pcsAndLines, and from linesToExplain. *)
-	(List.map
-		 (fun (pc,lines) -> pc,LineSet.diff lines linesControlledByVars)
-		 pcsAndLines,
-	 LineSet.diff linesToExplain linesControlledByVars)
+	(* Remove the lines that have been accounted for from linesToExplain. *)
+	LineSet.diff linesToExplain linesControlledByVars
 
 exception EverythingCovered
 
@@ -303,7 +292,7 @@ let calculateDeps pcsAndLines =
 			restResults
 	in
 
-Format.printf "%d (instead of %d) path conditions\n" (List.length pcsAndLines) !totalNumberOfPcs;
+Format.printf "%d path conditions\n" (List.length pcsAndLines);
 
 	(* Pick out variables that appear in some path condition, ignoring
 		 those that aren't mentioned anywhere. *)
@@ -326,9 +315,12 @@ Format.printf "%d (instead of %d) path conditions\n" (List.length pcsAndLines) !
 		(fun (file,line) -> Format.printf "%s:%d\n" file line)
 		alwaysExecuted;
 
-	(* Explain all the lines we can *)
-	let remainingPCsAndLines = ref pcsAndLines (* Initial path conditions and coverage information *)
-	and remainingLines = ref (LineSet.diff everExecuted alwaysExecuted) in (* All lines that weren't always executed *)
+	(* Remove lines that were always executed *)
+	let remainingPcsAndLines = ref
+		(List.rev_map
+			 (fun (pc,lines) -> (pc, LineSet.diff lines alwaysExecuted))
+			 pcsAndLines)
+	and remainingLines = ref (LineSet.diff everExecuted alwaysExecuted) in
 
 	(try
 		 let size = ref 1 in
@@ -337,20 +329,43 @@ Format.printf "%d (instead of %d) path conditions\n" (List.length pcsAndLines) !
 			 LineSet.iter
 				 (fun (file,lineNum) -> Format.printf "%s:%d\n" file lineNum)
 				 !remainingLines;
-			 if LineSet.is_empty !remainingLines
-			 then raise EverythingCovered;
-			 let (nextPcs,nextLines) =
+			 if !size = 3 || LineSet.is_empty !remainingLines then raise EverythingCovered;
+			 let linesStillUncovered =
 				 List.fold_left
-					 findDependenciesAndUpdate
-					 (!remainingPCsAndLines, !remainingLines)
+					 (fun lines b2v -> findDependenciesAndUpdate !remainingPcsAndLines lines b2v)
+					 !remainingLines
 					 (subsetsOfSize !size !bytesToVars)
 			 in
-			 remainingPCsAndLines := nextPcs;
-			 remainingLines := nextLines;
+			 let linesJustCovered = LineSet.diff !remainingLines linesStillUncovered in
+			 (* Update the pcsAndLines only after all subsets of a given
+					size have been examined. This enables us to see if there are
+					several ways of hitting guaranteeing coverage of the same
+					lines. *)
+			 remainingPcsAndLines :=
+				 List.rev_map
+					 (fun (pc,lines) -> (pc, LineSet.diff lines linesJustCovered))
+					 !remainingPcsAndLines;
+			 remainingLines := linesStillUncovered;
 			 incr size
 		 done
 	 with EverythingCovered -> ()
-	)
+	);
+
+	print_endline "numberOfHits";
+	for i = 1 to 4 do Format.printf "Size %d: %d\n" i numberOfHits.(i) done;
+	print_endline "numberOfUselessHits";
+	for i = 1 to 4 do Format.printf "Size %d: %d\n" i numberOfUselessHits.(i) done;
+	print_endline "numberOfSTPCalls";
+	for i = 1 to 4 do Format.printf "Size %d: %d\n" i numberOfSTPCalls.(i) done;
+	Format.printf "Optimization total time: %.2f s
+  It took %.2f s to construct the formulas for the expressions inside 'if(...)'s,
+  %.2f s to construct and assert the path conditions,
+  and %.2f s to solve the resulting formulas.\n\n"
+		(Stats.lookupTime "knownContradiction")
+		(Stats.lookupTime "convert conditional")
+		(Stats.lookupTime "STP assert")
+		(Stats.lookupTime "STP query");
+	()
 
 let treeOfPcs = ref (Node (rootNodeData, []))
 
@@ -400,7 +415,7 @@ let addCoverageFromFile filename =
 											 state.path_condition
 											 state.path_condition_tracked)
 			 in
-			 treeOfPcs := add pc hist.coveredLines !treeOfPcs)
+			 treeOfPcs := add (List.map simplifyLogicalOps pc) hist.coveredLines !treeOfPcs)
 		jobResults
 
 let feature : Cil.featureDescr = {
@@ -416,9 +431,6 @@ let feature : Cil.featureDescr = {
 		 "<filename> File from which to read the possible values for variables
 \t\t\tFormat is \"varname val_1 val_2 ... val_k\", with one variable per line,
 \t\t\tand the values must all be integers\n");
-		("--numVars",
-		 Arg.Set_int numVars,
-		 "<n> Consider n-tuples of variables (default: 1)\n");
 	];
   Cil.fd_doit = readInDataAndGo;
   Cil.fd_post_check = false;
