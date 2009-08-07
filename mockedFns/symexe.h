50aff656ac92697a8c25de739aa8a8d011bad3b2
Elnatan Reisner <elnatan@cs.umd.edu>
2009-08-07 16:46:52 +0000

    Changing the declared return type of OR() to int. (Making it char ended up making things more complicated.)

--- mockedFns/symexe.h
+++ mockedFns/symexe.h
@@ -2,8 +2,8 @@ extern long long __SYMBOLIC(void *var);
 extern void __EVAL(); // Don't specify parameters; this way, CIL won't cast them
 extern void __ASSERT();
 extern void __ASSUME();
-extern unsigned char OR();
-extern unsigned char AND();
+extern int OR();
+extern int AND();
 extern void __EVALSTR(char *str, int len);
 extern void __COMMENT(char *str);
 extern  __attribute__((__nothrow__, __noreturn__)) void exit(int __status );
