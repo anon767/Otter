1f580e8334cef0e99e64fa2ae8b4a485ba2b4d2d
Kin-Keung Ma <kkma@cs.umd.edu>
2009-08-14 03:28:31 +0000

    generate program code in html with uncovered lines highlighted examples in data/ngircd/html/

---
+++ experiments/uncovered.py
@@ -0,0 +1,57 @@
+import sys,os,cgi
+
+if len(sys.argv)<3:
+	print '''Usage: python uncovered.py diff_file path1 [path2 [...]]
+'''
+	sys.exit(0)
+	
+
+path = []
+def open_under_path(filename,mode):
+	for p in path:
+		if os.path.exists(p+filename):
+			print (p+filename)
+			return open(p+filename,mode)
+	raise Not_found
+
+	
+
+map = dict()
+def txt2html(filename):
+	file = open_under_path(filename,"r")
+	linenum = 0
+	output = [""]
+	
+	while True:
+		line = file.readline()
+		if line=="": break
+		linenum += 1
+		output.append(("%5d"%linenum)+ "\t"+ cgi.escape(line))
+	
+	file.close()
+	map[filename] = output
+	
+path = sys.argv[2:]
+file = open(sys.argv[1],"r")
+
+while True:
+	line = file.readline()
+	if line=="": break
+	(filename,str_linenum) = line.split(":")
+	linenum = int(str_linenum)
+	if filename not in map:
+		txt2html(filename)
+	map[filename][linenum] = "<span style=\"background-color: #ffff00\">"+map[filename][linenum][:-1]+"</span>\n"
+
+for (filename,lines) in map.items():
+	file = open(filename+".html","w")
+	print >>file, ("<html>")
+	print >>file, ("<body>")
+	print >>file, ("<pre>")
+	for line in lines:
+		if line=="": continue
+		print >> file, line,
+	print >>file, ("</pre>")
+	print >>file, ("</body>")
+	print >>file, ("</html>")
+	file.close()
