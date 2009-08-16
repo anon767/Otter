5710fe3b45227e2265d8a406538d0bbe14e221d9
Kin-Keung Ma <kkma@cs.umd.edu>
2009-08-16 18:48:29 +0000

    a bunch of new tests

---
+++ experiments/ngircd/codeCoverageTest/otherFiles/ngircd_13.c
@@ -0,0 +1,18 @@
+// ERROR
+int symtest(){
+	
+	int t;
+
+	// setup client(s)
+	int client_fd1 = socket(0,0,0);
+
+	t = 0;
+		
+	event_accept(client_fd1,t++);
+	event_recv(client_fd1,"PASS password 0210-IRC+ IRC|aBgH$ Z\r\n",t++);
+	event_recv(client_fd1,"SERVER irc2.the.net  1 :Experimental server\r\n",t++);
+	event_recv(client_fd1,"ERROR :error msg\r\n",t++);
+
+	event_end(t++);
+}
+
