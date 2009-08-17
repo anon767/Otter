93547679abece2c8cfde48a3d0355aa86cfa525b
Kin-Keung Ma <kkma@cs.umd.edu>
2009-08-17 17:03:33 +0000

    generate a table of summaries and links to the highlighted sources.
    I'm currently running tinyhttpd on skoll002 for the ease of accessing those htmls. e.g.,
    
    http://skoll002.umiacs.umd.edu:44444/ngircd/html/
    
    (you have to be in UMD network to access it)

--- experiments/uncovered.py
+++ experiments/uncovered.py
@@ -1,4 +1,5 @@
 import sys,os,cgi
+from collections import defaultdict
 
 if len(sys.argv)<3:
 	print '''Usage: python uncovered.py diff_file path1 [path2 [...]]
@@ -18,6 +19,8 @@ def open_under_path(filename,mode):
 	
 
 map = dict()
+count = defaultdict(int)
+
 def txt2html(filename):
 	file = open_under_path(filename,"r")
 	linenum = 0
@@ -42,20 +45,37 @@ for line in file:
 	if filename not in map:
 		txt2html(filename)
 	map[filename][linenum] = "<span style=\"background-color: #ffff00\">"+map[filename][linenum][:-1]+"</span>\n"
+	count[filename] += 1
 
-for (filename,lines) in map.items():
-	try:
-		os.mkdir("html")
-	except OSError:
-		pass
+file.close()
+
+try:
+	os.mkdir("html")
+except OSError:
+	pass
+
+index = open("html/index.html","w")
+print >>index, "<html>"
+print >>index, "<body>"
+print >>index, "<table>"
+print >>index, "<tr><td>File<td>Uncovered<td>Total&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<td>Ratio"
+
+for (filename,lines) in sorted(map.items(),key=lambda (f,l): -count[f]):
 	file = open("html/"+filename+".html","w")
-	print >>file, ("<html>")
-	print >>file, ("<body>")
-	print >>file, ("<pre>")
+	print >>file, "<html>"
+	print >>file, "<body>"
+	print >>file, "<pre>"
 	for line in lines:
 		if line=="": continue
 		print >> file, line,
-	print >>file, ("</pre>")
-	print >>file, ("</body>")
-	print >>file, ("</html>")
+	print >>file, "</pre>"
+	print >>file, "</body>"
+	print >>file, "</html>"
 	file.close()
+	print >>index, ("<tr><td><a href=\"./%s.html\">%s</a>" % (filename,filename)),
+	print >>index, ("<td>%d<td>%d<td>%.2f" % (count[filename],len(lines),float(count[filename])/len(lines))) 
+
+print >>index, "</table>"
+print >>index, "</body>"
+print >>index, "</html>"
+index.close()
