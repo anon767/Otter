830df564524f2746365832f9cbc9507eb3d0a57f
Kin-Keung Ma <kkma@cs.umd.edu>
2009-08-19 22:37:08 +0000

    changed some formatting

--- experiments/uncovered.py
+++ experiments/uncovered.py
@@ -1,8 +1,10 @@
 import sys,os,cgi
 from collections import defaultdict
 
+print '''Warning: Usage: python uncovered.py allLines_file diff_file output_dir path1 [path2 [...]]
+'''
 if len(sys.argv)<3:
-	print '''Usage: python uncovered.py diff_file path1 [path2 [...]]
+	print '''Usage: python uncovered.py allLines_file diff_file output_dir path1 [path2 [...]]
 '''
 	sys.exit(0)
 	
@@ -33,48 +35,76 @@ def txt2html(filename):
 	file.close()
 	map[filename] = output
 	
-path = sys.argv[2:]
-file = open(sys.argv[1],"r")
+output_dir = sys.argv[3]
+path = sys.argv[4:]
+file = open(sys.argv[2],"r")
 
+print "Path:"
 for p in path:
 	print p
+print ""
+
+# Get count of *total* lines for each file
+totalCounts = defaultdict(int)
+for line in open(sys.argv[1]):
+	(filename,str_linenum) = line.split(":")
+	linenum = int(str_linenum)
+	if filename not in map:
+		txt2html(filename)
+	map[filename][linenum] = "<font color=\"#000000\">"+map[filename][linenum][:-1]+"</font>\n"
+	totalCounts[filename] += 1
+
 
 for line in file:
 	(filename,str_linenum) = line.split(":")
 	linenum = int(str_linenum)
 	if filename not in map:
-		txt2html(filename)
+		raise abc
 	map[filename][linenum] = "<span style=\"background-color: #ffff00\">"+map[filename][linenum][:-1]+"</span>\n"
 	count[filename] += 1
 
 file.close()
 
 try:
-	os.mkdir("html")
+	os.mkdir(output_dir)
 except OSError:
 	pass
 
-index = open("html/index.html","w")
+index = open(output_dir+"/index.html","w")
 print >>index, "<html>"
 print >>index, "<body>"
 print >>index, "<table>"
 print >>index, "<tr><td>File<td>Uncovered<td>Total&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<td>Ratio"
 
+total_uncovered = 0
+total_total = 0
+
+def percentage(a,b):
+	return "%.0f%%" % (float(a)*100/b)
+def fix(f):
+	g = f.replace("/","__SLASH__")
+	return g
+
 for (filename,lines) in sorted(map.items(),key=lambda (f,l): -count[f]):
-	file = open("html/"+filename+".html","w")
+	file = open(output_dir+"/"+fix(filename)+".html","w")
 	print >>file, "<html>"
 	print >>file, "<body>"
 	print >>file, "<pre>"
+	print >>file, "<font color=\"#BBBBBB\">"
 	for line in lines:
 		if line=="": continue
 		print >> file, line,
+	print >>file, "</font>"
 	print >>file, "</pre>"
 	print >>file, "</body>"
 	print >>file, "</html>"
 	file.close()
-	print >>index, ("<tr><td><a href=\"./%s.html\">%s</a>" % (filename,filename)),
-	print >>index, ("<td>%d<td>%d<td>%.2f" % (count[filename],len(lines),float(count[filename])/len(lines))) 
+	print >>index, ("<tr><td><a href=\"./%s.html\">%s</a>" % (fix(filename),filename)),
+	print >>index, ("<td>%d<td>%d<td>%s" % (count[filename],totalCounts[filename],percentage(count[filename],totalCounts[filename]))) 
+	total_uncovered += count[filename]
+	total_total += totalCounts[filename]
 
+print >>index, ("<tr><td>Total: <td>%d<td>%d<td>%s" % (total_uncovered,total_total,percentage(total_uncovered,total_total))) 
 print >>index, "</table>"
 print >>index, "</body>"
 print >>index, "</html>"
