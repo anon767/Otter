7d28dd69bc1ef934219bf033a39f7df88cecb5c0
Charles Song <csfalcon@umd.edu>
2009-08-19 18:37:05 +0000

    Additional grep test cases.

---
+++ experiments/grep/otherFiles/charles_11.c
@@ -0,0 +1,20 @@
+#include "iosim.h"
+#include <stdlib.h>
+
+void symtest_initialize() {
+	IOSIM_fd[1] = malloc(sizeof(sym_file_stream_t));
+	IOSIM_fd[1]->offset = 0;
+	IOSIM_fd[1]->fd = 1;
+	IOSIM_fd[1]->sym_file = malloc(sizeof(sym_file_t));
+	IOSIM_fd[1]->sym_file->contents = NULL;
+	IOSIM_fd[1]->sym_file->stat.st_size = 0;
+	stdout = IOSIM_fd[1];
+
+	sym_file_t* input1 = IOSIM_addfile("/grep/charles_11_1.txt", 0);
+	input1->contents = "abc";
+	input1->stat.st_size = 3;
+
+	sym_file_t* input2 = IOSIM_addfile("/grep/charles_11_2.txt", 0);
+        input2->contents = "123";
+        input2->stat.st_size = 3;
+}
---
+++ experiments/grep/otherFiles/charles_29.c
@@ -0,0 +1,16 @@
+#include "iosim.h"
+#include <stdlib.h>
+
+void symtest_initialize() {
+	IOSIM_fd[1] = malloc(sizeof(sym_file_stream_t));
+	IOSIM_fd[1]->offset = 0;
+	IOSIM_fd[1]->fd = 1;
+	IOSIM_fd[1]->sym_file = malloc(sizeof(sym_file_t));
+	IOSIM_fd[1]->sym_file->contents = NULL;
+	IOSIM_fd[1]->sym_file->stat.st_size = 0;
+	stdout = IOSIM_fd[1];
+
+	sym_file_t* input = IOSIM_addfile("/charles_29.txt", 0);
+	input->contents = "abc";
+	input->stat.st_size = 3;
+}
