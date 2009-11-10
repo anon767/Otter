baa580cf547136a6d3bec212d148c371f84ce6ba
Kin-Keung Ma <kkma@cs.umd.edu>
2009-11-10 03:57:00 +0000

    removed the local header directory. will replace it by an archive.

--- includes/usr/include/ncurses.h
+++
@@ -1,1 +0,0 @@
-/usr/include/ncurses/curses.h
--- includes/usr/include/pi-header.h
+++
@@ -1,5 +0,0 @@
-/* Print the version splash 	*/
-void print_splash(char *progname);
-
-/* Connect to the Palm device	*/
-int pilot_connect(char *port);
