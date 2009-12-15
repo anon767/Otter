0d6941fe2fedd91ba272cb3ea8e3b578d5e81316
Kin-Keung Ma <kkma@cs.umd.edu>
2009-12-15 01:14:10 +0000

    a "slow" test

---
+++ cil/setest/nested-table.c
@@ -0,0 +1,16 @@
+// ./run setest/nested-table.c 
+
+#define MAX 300
+int f[MAX];
+
+void main(){
+
+	int k;
+	int i = __SYMBOLIC();
+	__ASSUME(i>=0,i<MAX);
+
+	for(k=0;k<MAX;++k)
+		f[k]=k;
+
+	__ASSERT(f[f[f[f[i]]]] >=0);
+}
