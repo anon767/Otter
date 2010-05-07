8522bebb027da476da8b09ae978622a72fb339ac
Kin-Keung Ma <kkma@cs.umd.edu>
2010-05-07 15:17:48 +0000

    prioritize using the distance to targets

--- otter/src/Otter/prioritizer.ml
+++ otter/src/Otter/prioritizer.ml
@@ -15,41 +15,40 @@ let fifo job = -. (timer ()) ;;
 
 
 
-(**
-  * Prioritization using CFG
-  *)
+module InstrStmt =
+struct
+  type instr_stmt =
+    | Instr of Cil.instr list * Cil.stmt (* stmt: stmt that contains instr *)
+    | Stmt of Cil.stmt
+  ;;
+  let instr_stmt_length instr_stmt =
+    match instr_stmt with
+      | Stmt (stmt) -> List.length stmt.succs
+      | _ -> 1
+  ;;
+  let make_instr_stmt_Stmt stmt =
+    Stmt (stmt)
+  ;;
+  let make_instr_stmt_Instr (instrs,stmt) =
+    Instr (instrs,stmt)
+  ;;
+  let get_instr_stmt job =
+    match job.instrList with
+      | instr::instrs -> 
+          make_instr_stmt_Instr (job.instrList,List.hd job.stmt.preds)
+      | [] -> make_instr_stmt_Stmt (job.stmt)
+  ;;
+  let print_instr_stmt instr_stmt =
+    match instr_stmt with
+      | Instr (instrs,stmt) -> "(Instr:"^(List.fold_right (fun instr str -> (To_string.instr instr)^"::"^str) instrs "[]" )^","^(To_string.stmt stmt)^")"
+      | Stmt (stmt) -> "(Stmt:"^(To_string.stmt stmt)^")"
+  ;;
+end;;
 
-type instr_stmt =
-  | Instr of Cil.instr list * Cil.stmt (* stmt: stmt that contains instr *)
-  | Stmt of Cil.stmt
-;;
+open InstrStmt
 
-let instr_stmt_length instr_stmt =
-  match instr_stmt with
-    | Stmt (stmt) -> List.length stmt.succs
-    | _ -> 1
-;;
-
-let hash_consing_instr_stmt_tbl : (instr_stmt,instr_stmt) Hashtbl.t = Hashtbl.create 10;;
-let hash_consing_instr_stmt_create bs = 
-  try Hashtbl.find hash_consing_instr_stmt_tbl bs
-  with Not_found -> Hashtbl.add hash_consing_instr_stmt_tbl bs bs; bs;;
-
-let make_instr_stmt_Stmt stmt =
-  hash_consing_instr_stmt_create (Stmt (stmt))
-;;
-
-let make_instr_stmt_Instr (instrs,stmt) =
-  hash_consing_instr_stmt_create (Instr (instrs,stmt))
-;;
-
-let get_instr_stmt job =
-  match job.instrList with
-    | instr::instrs -> 
-        make_instr_stmt_Instr (job.instrList,List.hd job.stmt.preds)
-    | [] -> make_instr_stmt_Stmt (job.stmt)
-;;
-
+module Graph =
+struct
 type graph_node = 
     { 
         mutable obj: instr_stmt;
@@ -60,13 +59,6 @@ type graph_node =
 ;;
 
 type graph = (instr_stmt,graph_node) Hashtbl.t ;;
-
-let print_instr_stmt instr_stmt =
-  match instr_stmt with
-    | Instr (instrs,stmt) -> "(Instr:"^(List.fold_right (fun instr str -> (To_string.instr instr)^"::"^str) instrs "[]" )^","^(To_string.stmt stmt)^")"
-    | Stmt (stmt) -> "(Stmt:"^(To_string.stmt stmt)^")"
-;;
-
 let print_node node =
   let s = Printf.sprintf "Node: %s\n" (print_instr_stmt node.obj) in
     List.fold_left 
@@ -77,18 +69,15 @@ let print_node node =
       end
       s node.nexts
 ;;
-
 let print_graph graph =
   To_string.force_print := true;
   Hashtbl.iter (fun k v -> Printf.printf "%s" (print_node v)) graph;
   To_string.force_print := true;
   ()
 ;;
-
 let graph__set_color graph color =
   Hashtbl.iter (fun _ node -> node.color <-color) graph
 ;;
-
 let make_graph_node graph instr_stmt =
   (* memoization *)
   if Hashtbl.mem graph instr_stmt then
@@ -118,6 +107,9 @@ let make_graph fundec =
                 | Cil.Instr (instrs) -> 
                     let succ = make_instr_stmt_Instr (instrs,stmt) in
                       explore_succ graph node succ
+                | Cil.Loop _ ->
+                    assert(List.length stmt.succs = 1);
+                    List.iter (explore_succ graph node) (List.map make_instr_stmt_Stmt stmt.succs)
                 | _ -> 
                     List.iter (explore_succ graph node) (List.map make_instr_stmt_Stmt stmt.succs)
               end
@@ -162,21 +154,8 @@ let graph__get_nodes_satisfying graph predicate =
     graph []
 ;;
 
-(*
-let graph__get_node graph instr_stmt =
-  Printf.printf "-----------------------------------------------\n";
-  Printf.printf "%s\n" (print_instr_stmt instr_stmt);
-  print_graph graph;
-  let result = Hashtbl.find graph instr_stmt in
-    Printf.printf "found.\n";
-    result
-;;
- *)
-
-
-let reachable graph src tar =
-  graph__set_color graph 0;
-  let rec dfs graph tar src =
+let backward_reachable graph src tar =
+  let rec dfs tar =
     if tar == src then true
     else if tar.color = 1 then false 
     else
@@ -184,12 +163,42 @@ let reachable graph src tar =
         tar.color <- 1;
         List.fold_left 
           begin
-            fun t prev -> if t then true else dfs graph prev src
+            fun t prev -> if t then true else dfs prev 
           end false tar.prevs
       end
   in
-    dfs graph tar src 
+    graph__set_color graph 0;
+    dfs tar 
 ;;
+let backward_distance graph src tar =
+  let rec bfs tars =
+    let tars',srcdist = List.fold_left 
+      begin
+        fun (lst,dist) tar ->
+          List.fold_left
+            begin
+              fun (lst,dist) prev ->
+                if dist < max_int then ([],dist) else
+                let _ = if prev.color = max_int then
+                  prev.color <- tar.color + 1
+                else () in
+                  if prev == src then ([],src.color) else (prev::lst,dist)
+            end
+            (lst,dist) tar.prevs
+      end
+      ([],max_int) tars
+    in
+      if srcdist < max_int then srcdist 
+      else if tars' = [] then max_int 
+      else bfs tars'
+  in
+    graph__set_color graph max_int;
+    tar.color <- 0;
+    bfs [tar] 
+;;
+end;;
+
+open Graph
 
 let prioritize job = 
   if (Executeargs.run_args.Executeargs.arg_cfg_pruning) then
@@ -214,15 +223,24 @@ let prioritize job =
       let sources = graph__get_nodes_satisfying graph (get_predicate job) in
       assert(List.length sources = 1);
       let source = List.hd sources in
-      let reachable_from_targets = List.fold_left
+    (*
+      let backward_reachable_from_targets = List.fold_left
                                         (fun t tar -> if t then true else
-                                           reachable graph source tar)
+                                           backward_reachable graph source tar)
                                      false targets
       in
-        if reachable_from_targets then
+        if backward_reachable_from_targets then
           lifo job
         else 
           neg_infinity
+     *)
+      let backward_distance_from_targets = List.fold_left
+                                             (fun d tar -> 
+                                                let d' = backward_distance graph source tar in
+                                                  min d d'
+                                             )
+                                             max_int targets
+      in -. (float_of_int backward_distance_from_targets)
     end
   else 
     lifo job
