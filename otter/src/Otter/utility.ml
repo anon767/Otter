833b6b94d3a10c6911a2cda150ec3c19d122bba6
Khoo Yit Phang <khooyp@cs.umd.edu>
2010-07-31 03:19:04 +0000

    Ditch unused functions Utility.add_all and Utility.colorize with associated code.

--- otter/src/Otter/utility.ml
+++ otter/src/Otter/utility.ml
@@ -4,13 +4,6 @@ module IndexMap = Map.Make (struct
 	let compare (a : int) (b : int) = Pervasives.compare a b
 end)
 
-let rec add_all add_function keys values map = 
-	match (keys,values) with
-		| ([],[]) -> map
-		| (k::ks,v::vs) -> let map2 = add_function k v map in add_all add_function ks vs map2
-		| _ -> raise (Failure "wow")
-
-
 let increment iref =
   iref:=(!iref)+1
 
@@ -35,13 +28,4 @@ let getchar () =
 module StringSet = Set.Make(String)
 
 let errormsg e = match e with Failure s -> s | _ -> "Unknown error"
-
-type color = { color_code: string }
-
-let red = {color_code="31"}
-let green = {color_code="32"}
-let blue = {color_code="34"}
-
-let colorize color text =
-  Printf.sprintf "\x1b[%sm%s\x1b[m" color.color_code text
 
