bd3499f6545652520bcf88f1addcb920750c78aa
Khoo Yit Phang <khooyp@cs.umd.edu>
2010-08-04 14:19:11 +0000

    Add OcamlUtilities as a dependency to runtestotter.

--- otter/test/runtestotter.ml
+++ otter/test/runtestotter.ml
@@ -1,8 +1,11 @@
 open TestUtil.MyOUnit
 
-(* ocamlbuild's dynamic dependency is broken: turn the below into static dependencies *)
+(* There's something wrong with ocamlbuild's dynamic dependency that causes it to miss some transitive dependencies
+    from one mlpack to another mlpack. A workaround is to make sure all mlpacks are listed somewhere in this file:
+    either in working code, or in the dummy module below. *)
 module OcamlbuildDependencies = struct
     open Otter
+    open OcamlUtilities
 end
 
 let _ =
