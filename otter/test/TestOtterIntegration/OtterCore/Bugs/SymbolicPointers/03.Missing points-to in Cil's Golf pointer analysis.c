001d66edac6781e535f1d730fb4ca21d1816b26a
Khoo Yit Phang <khooyp@cs.umd.edu>
2010-10-14 04:47:52 +0000

    Add a failing test case due to an odd bug in Cil's Golf pointer analysis.

---
+++ otter/test/TestOtterIntegration/OtterCore/Bugs/SymbolicPointers/03.Missing points-to in Cil's Golf pointer analysis.c
@@ -0,0 +1,39 @@
+/*  This test reveals an odd bug in Cil's Golf pointer analysis, which for some reason does not pick up that y points
+    to the malloc()'ed memory in main() (y == *w == z == malloc). Because x points to y, all three branches in foo()
+    should be feasible, but the third one is missed if Golf is used.
+*/
+
+#pragma entry_function("foo")
+#pragma expect_return(__exit_code__ == 0)
+#pragma expect_return(__exit_code__ == 1)
+#pragma expect_return(__exit_code__ == 2)
+#pragma no_other_results
+
+typedef unsigned long size_t;
+void * malloc(size_t size);
+
+typedef struct dll {
+    struct dll *next;
+} dll;
+
+int foo(dll **x) {
+    if (!x) {
+        return 0;
+    } else if (*x == 0) {
+        return 1;
+    } else {
+        return 2; /* this line doesn't get executed if Golf is used */
+    }
+    (*x)->next = 0; /* the bug disappears if we omit this line */
+}
+
+void bar(dll **w) {
+    dll *y = *w;
+    foo(&y);
+}
+
+void main(void) {
+    dll *z = malloc(sizeof(dll));
+    bar(&z);
+}
+
