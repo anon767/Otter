6f8d1200343810cb21982dfc18c6c8f89405616d
Khoo Yit Phang <khooyp@cs.umd.edu>
2010-10-18 19:18:32 +0000

    Inline Cilly.makeCFGFeature.Cil.fd_doit, to make it clear what OtterCore requires.

--- otter/src/OtterCore/Core.ml
+++ otter/src/OtterCore/Core.ml
@@ -2,22 +2,19 @@ open CilUtilities
 
 (* set up the file for symbolic execution *)
 let prepare_file file =
-	(* makeCFGFeature must precede the call to Coverage.prepare_file. *)
-	Cilly.makeCFGFeature.Cil.fd_doit file;
+    (* based on Cilly.makeCFGFeature.fd_doit *)
+    ignore (Partial.calls_end_basic_blocks file);
+    ignore (Partial.globally_unique_vids file);
+    Cil.iterGlobals file begin function
+        | Cil.GFun(fd,_) ->
+            Cil.prepareCFG fd; (* reduce switch/break/default/continue to if/goto *)
+            ignore (Cil.computeCFGInfo fd false) (* false: per-function statement ids, required for Coverage *)
+        | _ ->
+            ()
+    end;
 
-	Cil.iterGlobals file begin function
-		| Cil.GFun(fundec,_) ->
-			(* Reset sids to be unique only within a function, rather than globally, so that they will be given
-				consistent sids across different analysis runs even if different files are merged
-			*)
-			ignore (List.fold_left (fun n stmt -> stmt.Cil.sid <- n; succ n) 0 fundec.Cil.sallstmts);
+    if !Executeargs.arg_noinit_unreachable_globals then
+        Coverage.computeReachableCode file;
 
-		| _ ->
-			()
-	end;
-
-	if !Executeargs.arg_noinit_unreachable_globals then
-		Coverage.computeReachableCode file;
-
-	Coverage.prepare_file file
+    Coverage.prepare_file file
 
