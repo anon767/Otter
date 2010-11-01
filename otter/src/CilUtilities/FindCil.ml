5b19423aa2890b5c55a745921fcc06c1a4a8be26
Elnatan Reisner <elnatan@cs.umd.edu>
2010-11-01 17:59:58 +0000

    Adding some error messages in FindCil. (I couldn't figure out how to use FormatPlus from FindCil.)

--- otter/src/CilUtilities/FindCil.ml
+++ otter/src/CilUtilities/FindCil.ml
@@ -47,7 +47,8 @@ let globals_memotables =
 		@raise Not_found if a {!Cil.fundec} for [varinfo] does not exist in [file]
 *)
 let fundec_by_varinfo file varinfo =
-	(globals_memotables file)#varinfo_to_fundec varinfo
+    try (globals_memotables file)#varinfo_to_fundec varinfo
+    with Not_found -> failwith ("Cannot find fundec for varinfo " ^ varinfo.Cil.vname)
 
 
 (** Find the {!Cil.initinfo} for a {!Cil.varinfo} from a {!Cil.file}.
@@ -57,7 +58,8 @@ let fundec_by_varinfo file varinfo =
 		@raise Not_found if a global [varinfo] does not exist in [file]
 *)
 let global_varinit_by_varinfo file varinfo =
-	(globals_memotables file)#varinfo_to_varinit varinfo
+    try (globals_memotables file)#varinfo_to_varinit varinfo
+    with Not_found -> failwith ("Cannot find initialization value for varinfo " ^ varinfo.Cil.vname)
 
 
 (** Find a {!Cil.fundec} by name from a {!Cil.file}.
@@ -67,7 +69,8 @@ let global_varinit_by_varinfo file varinfo =
 		@raise Not_found if a {!Cil.fundec} named [name] does not exist in [file]
 *)
 let fundec_by_name file name =
-	(globals_memotables file)#name_to_fundec name
+    try (globals_memotables file)#name_to_fundec name
+    with Not_found -> failwith ("Cannot find fundec for " ^ name)
 
 
 (** Find a global {!Cil.varinfo} by name from a {!Cil.file}.
@@ -77,6 +80,5 @@ let fundec_by_name file name =
 		@raise Not_found if a global {!Cil.varinfo} named [name] does not exist in [file]
 *)
 let global_varinfo_by_name file name =
-	(globals_memotables file)#name_to_global_varinfo name
-
-
+    try (globals_memotables file)#name_to_global_varinfo name
+    with Not_found -> failwith ("Cannot varinfo for " ^ name)
