69c9b776528f3f39d6db60f1e561096623ba3e8f
Jonathan Turpie <yudi@cs.umd.edu>
2010-11-01 04:53:50 +0000

    Added test cases for fork behavior.

---
+++ otter/test/TestOtterSystem/MultiProcessOtter/libc/open_persists_after_fork.c
@@ -0,0 +1,27 @@
+#pragma no_other_abandoned
+
+#include <unistd.h>
+
+#define STR "hello"
+#define LEN sizeof(STR)-1
+
+int main() {
+	__otter_libc_init();
+    char filename[] = "a";
+	int fd = open(filename, O_CREAT | O_RDWR, S_IRWXU);
+	if (fork() == 0) {
+		char buf[10];
+		strcpy(buf, STR);
+		int bytes_written = write(fd, buf, LEN);
+		__ASSERT(bytes_written == LEN);
+	} else {
+		for (int i = 0; i < 150; i++); // Delay so that the write happens first
+		lseek(fd, 0, SEEK_SET); //seek back to the beginning of the file
+		char buf[] = "abcdefghi";
+		int bytes_read = read(fd, buf, 10);
+		__ASSERT(bytes_read == LEN);
+		int cmp = strcmp(buf, "hellofghi");
+		__ASSERT(cmp == 0);
+	}
+  return 0;
+}
