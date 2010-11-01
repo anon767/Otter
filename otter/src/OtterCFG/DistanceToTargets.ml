82c6aa5f7be1aac709f3b4380e65c00f940a03f8
Khoo Yit Phang <khooyp@cs.umd.edu>
2010-11-01 19:49:03 +0000

    Fix several bugs in DistanceToTargets: - fix InstructionTargetsHash.equal to handle unequal instr list; - disallow non-empty target list; - fix non-termination because visited instructions weren't always being marked (similar to r10216).
    Also, distances are now only compute through function calls, but not through function returns.

--- otter/src/OtterCFG/DistanceToTargets.ml
+++ otter/src/OtterCFG/DistanceToTargets.ml
@@ -3,7 +3,7 @@
 module InstructionSet = Set.Make (Instruction)
 module InstructionTargetsHash = Hashtbl.Make (struct
     type t = Instruction.t * Instruction.t list
-    let equal (x, xt) (y, yt) = List.for_all2 Instruction.equal (x::xt) (y::yt)
+    let equal (x, xt) (y, yt) = try List.for_all2 Instruction.equal (x::xt) (y::yt) with Invalid_argument "List.for_all2" -> false
     let hash (x, xt) = List.fold_left (fun h x -> 33 * h + Instruction.hash x) 0 (x::xt)
 end)
 (**/**)
@@ -12,6 +12,7 @@ end)
 let find =
     let memotable = InstructionTargetsHash.create 0 in
     fun instr targets ->
+        if targets = [] then invalid_arg "find: targets must be a non-empty list";
         try
             InstructionTargetsHash.find memotable (instr, targets)
 
@@ -27,26 +28,33 @@ let find =
             in
             let rec update worklist =
                 let instr = InstructionSet.choose worklist in
-                let dist = try InstructionTargetsHash.find memotable (instr, targets) with Not_found -> max_int in
-
-                (* compute the new distance by taking 1 + the minimum distance of its successors + the minimum distance
-                   of its call targets, or the minimum distance of its return targets if it's a return instruction,
-                   adding uncomputed successors and call targets or return targets to the worklist *)
-                let worklist' = worklist in
-                let dist', worklist' = match Instruction.return_targets instr with
-                    | [] ->
-                        (* not a return instruction *)
-                        let succ_dist, worklist' = calc_dist (Instruction.successors instr) worklist' in
-                        let target_dist, worklist' = calc_dist (Instruction.call_targets instr) worklist' in
-                        let dist' = succ_dist + target_dist in
-                        let dist' = if dist' > 0 then dist' else max_int in (* overflow *)
-                        (dist', worklist')
-                    | instrs ->
-                        calc_dist instrs worklist'
+                let dist =
+                    try
+                        InstructionTargetsHash.find memotable (instr, targets)
+                    with Not_found ->
+                        InstructionTargetsHash.add memotable (instr, targets) max_int;
+                        max_int
                 in
+
+                (* compute the new distance by taking the minimum of:
+                        - 1 + the minimum distance of its successors + the minimum distance through its call targets,
+                        - or, 1 + the minimum distance of it's call targets;
+                   adding uncomputed successors and call targets to the worklist *)
+                let worklist' = worklist in
+                let targets = Instruction.call_targets instr in
+                let succ_dist, worklist' =
+                    let succ_dist, worklist' = calc_dist (Instruction.successors instr) worklist' in
+                    let succ_dist = match targets with
+                        | [] -> succ_dist
+                        | targets -> succ_dist + List.fold_left (fun d target -> min d (DistanceToReturn.find target)) max_int targets
+                    in
+                    let succ_dist = if succ_dist < 0 then dist (* overflow *) else succ_dist in
+                    (succ_dist, worklist')
+                in
+                let target_dist, worklist' = calc_dist targets worklist' in
                 let dist' =
-                    let dist' = 1 + dist' in
-                    if dist' > 0 then dist' else max_int (* overflow *)
+                    let dist' = 1 + min succ_dist target_dist in
+                    if dist' < 0 then dist (* overflow *) else dist'
                 in
                 (* update the distance if changed *)
                 if dist' <> dist then InstructionTargetsHash.replace memotable (instr, targets) dist';
@@ -56,16 +64,10 @@ let find =
                     if not (InstructionSet.equal worklist' worklist) then worklist' else
 
                     (* if the worklist is not updated and the distance has changed, then add the predecessors and
-                       call sites, or the return targets if it is a call instruction, to the worklist. *)
+                       call sites to the worklist. *)
                     if dist' <> dist then
-                        let instrs = match Instruction.return_sites instr with
-                            | [] ->
-                                (* not a call instruction *)
-                                List.rev_append (Instruction.predecessors instr) (Instruction.call_sites instr)
-                            | instrs ->
-                                instrs
-                        in
-                        List.fold_left (fun worklist instr -> InstructionSet.add instr worklist) worklist instrs
+                        List.fold_left (fun worklist instr -> InstructionSet.add instr worklist) worklist
+                            (List.rev_append (Instruction.predecessors instr) (Instruction.call_sites instr))
                     else
                         worklist
                 in
