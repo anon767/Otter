d249442e847680b5d60eec34c0a2f3e7d222fdf5
Khoo Yit Phang <khooyp@cs.umd.edu>
2010-11-02 02:50:46 +0000

    Get rid of an infinite recursion in TestBackOtter/otter.h.

--- otter/test/TestBackOtter/otter.h
+++ otter/test/TestBackOtter/otter.h
@@ -2,7 +2,7 @@
 #ifndef __OTTER_H__
 #define __OTTER_H__
 #include "__otter/all.h"
-int __COMMENT() { __COMMENT("Body of __COMMENT"); return 0;}
+int __COMMENT() { return 0;}
 int __ASSERT() { __COMMENT("Body of __ASSERT"); return 0;}
 int __SYMBOLIC() { __COMMENT("Body of __SYMBOLIC"); return 0;}
 int __EVAL() { __COMMENT("Body of __EVAL"); return 0;}
