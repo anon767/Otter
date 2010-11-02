6214debb94f037c1c32c647df0b3bab17bea6ce5
Jonathan Turpie <yudi@cs.umd.edu>
2010-11-02 07:21:28 +0000

    Copied in code for signal handlers and output into MultiOtter.

--- otter/src/MultiOtter/MultiDriver.ml
+++ otter/src/MultiOtter/MultiDriver.ml
@@ -94,10 +94,24 @@ let run reporter job =
 		[ multijob ]
 
 let doit file =
-	(* TODO: do something about signal handlers/run statistics from Executemain.doExecute *)
-
 	(* connect Cil's debug flag to Output *)
 	Output.arg_print_debug := !Errormsg.debugFlag;
+	
+	Output.printf "MultiOtter, a multiprocess symbolic executor for C@\n@\n";
+
+	(* Keep track of how long we run *)
+	let startTime = Unix.gettimeofday () in
+
+	(* Set signal handlers to catch timeouts and interrupts *)
+	let old_ALRM_handler =
+		Sys.signal Sys.sigalrm
+			(Sys.Signal_handle (fun _ -> raise (SignalException "Timed out!")))
+	and old_INT_handler =
+		Sys.signal Sys.sigint
+			(Sys.Signal_handle (fun _ -> raise (SignalException "User interrupt!")))
+	in
+	(* Set a timer *)
+	ignore (Unix.alarm !Executeargs.arg_timeout);
 
 	Core.prepare_file file;
 	let job = OtterJob.Job.get_default file in
@@ -105,8 +119,36 @@ let doit file =
 	(* run the job *)
 	let module Reporter = ErrorReporter.Make (OtterCore.Errors) in
 	let result = run (new Reporter.t ()) job in
+	
+	(* Turn off the alarm and reset the signal handlers *)
+	ignore (Unix.alarm 0);
+	Sys.set_signal Sys.sigalrm old_ALRM_handler;
+	Sys.set_signal Sys.sigint old_INT_handler;
 
 	(* print the results *)
+	Output.set_formatter (new Output.plain);
+	Output.printf "%s@\n" (Executedebug.get_log ());
+	Output.printf "\nSTP was invoked %d times (%d cache hits).\n" !Stp.stp_count !Stp.cacheHits;
+
+	let executionTime = (Unix.gettimeofday ()) -. startTime
+	and stpTime = Stats.lookupTime "STP" in
+	Output.printf "It ran for %.2f s, which is %.2f%% of the total %.2f s execution.\n"
+		stpTime (100. *. stpTime /. executionTime) executionTime;
+	Output.printf "  It took %.2f s to construct the formulas for the expressions inside 'if(...)'s,
+  %.2f s to construct and %.2f s to assert the path conditions,
+  and %.2f s to solve the resulting formulas.\n\n"
+		(Stats.lookupTime "convert conditional")
+		(Stats.lookupTime "STP construct")
+		(Stats.lookupTime "STP doassert")
+		(Stats.lookupTime "STP query");
+	(if !Executeargs.arg_simplify_path_condition then
+		Output.printf "It took %.2f s to simplify path conditions.\n"
+			(Stats.lookupTime "Simplify PC")
+	else ());
+
+	Output.printf "Hash-consing: hits=%d misses=%d\n" (!Bytes.hash_consing_bytes_hits) (!Bytes.hash_consing_bytes_misses);
+	Output.printf "Bytes eval caching: hits=%d misses=%d\n\n" (!MemOp.bytes_eval_cache_hits) (!MemOp.bytes_eval_cache_misses);
+
 	Output.printf "%s@\n" (Executedebug.get_log ());
 	Report.print_report result#completed
 
