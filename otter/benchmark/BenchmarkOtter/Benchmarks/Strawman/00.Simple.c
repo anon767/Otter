6b92e47be02c43d80b7e1b20ab66735423069c56
Khoo Yit Phang <khooyp@cs.umd.edu>
2010-11-02 14:51:45 +0000

    Reorganize benchmarks into directories (just one now).

--- otter/benchmark/BenchmarkOtter/Benchmarks/Strawman/00.Simple.c
+++ otter/benchmark/BenchmarkOtter/Benchmarks/Strawman/00.Simple.c
@@ -23,7 +23,7 @@
  *
  * -----------------------------------------
  */
-
+ 
 #pragma expect_abandoned(failure_reached, x > -5)
 #pragma expect_abandoned(failure_reached, x <= -5)
 #pragma expect_return()
