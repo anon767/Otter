d849ebfd11b1af0e45c0e5ee176923b17e9b943d
Khoo Yit Phang <khooyp@cs.umd.edu>
2010-11-02 19:05:02 +0000

    Another bug case for BackOtter.

---
+++ otter/test/TestOtterIntegration/BackOtter/Bugs/01.Call builtin function with no body.c
@@ -0,0 +1,14 @@
+/*
+ * This test ensures that during BackOtter's callgraph processing, built-in functions, which have no body, are handled
+ * appropriately.
+ *
+ * As of r10258, this test causes BackOtter to crashes.
+ */
+
+int __SYMBOLIC(void *);
+void __FAILURE(void) { }
+
+int main(void) {
+    __SYMBOLIC();
+    return 0;
+}
