8af9ba28acf1d027dccb1e79725cc0781b6061fb
Khoo Yit Phang <khooyp@cs.umd.edu>
2010-11-04 18:08:21 +0000

    Minor rename.

--- otter/src/OtterQueue/LeastCoveredQueue.ml
+++ otter/src/OtterQueue/LeastCoveredQueue.ml
@@ -14,31 +14,31 @@ class ['job] t = object
 
     method put (job : 'job) =
         (* score and enqueue *)
-        let instr_id = Job.get_instruction job in
+        let instr = Job.get_instruction job in
         let count, jobs =
             try
-                InstructionPriority.lookup instr_id queue
+                InstructionPriority.lookup instr queue
             with InstructionPriority.Key ->
-                let count = try InstructionMap.find instr_id coverage with Not_found -> 0 in
+                let count = try InstructionMap.find instr coverage with Not_found -> 0 in
                 (count, RandomBag.empty)
         in
         let jobs = RandomBag.put job jobs in
-        let queue = InstructionPriority.insert instr_id count jobs queue in
+        let queue = InstructionPriority.insert instr count jobs queue in
         {< queue = queue >}
 
     method get =
         try
             (* update coverage and dequeue *)
-            let instr_id, count, jobs = InstructionPriority.find_min queue in
+            let instr, count, jobs = InstructionPriority.find_min queue in
             match RandomBag.get jobs with
                 | Some (jobs, job) ->
                     let count = count + 1 in
-                    let coverage = InstructionMap.add instr_id count coverage in
+                    let coverage = InstructionMap.add instr count coverage in
                     let queue =
                         if RandomBag.is_empty jobs then
                             InstructionPriority.delete_min queue
                         else
-                            InstructionPriority.insert instr_id count jobs queue
+                            InstructionPriority.insert instr count jobs queue
                     in
                     Some ({< coverage = coverage; queue = queue >}, job)
                 | None ->
