60d832c64feb87bf26bd27b039a6b99eba1d4799
Khoo Yit Phang <khooyp@cs.umd.edu>
2010-11-05 14:57:55 +0000

    Remove a stray printf.

--- otter/benchmark/BenchmarkUtil/BackOtterBenchmarkingReporter.ml
+++ otter/benchmark/BenchmarkUtil/BackOtterBenchmarkingReporter.ml
@@ -11,7 +11,6 @@ class ['self] t ?max_nodes ?max_paths ?max_abandoned () = object
             super#report result
 
         | Job.Complete (Job.Abandoned (_, _, _)) when not (!BackOtterReporter.arg_exceptions_as_failures)->
-            Format.printf "OMG You are here!";
             (* munge the statistics then call super indirectly *)
             {< abandoned = abandoned - 1 >}#super_report result
 
