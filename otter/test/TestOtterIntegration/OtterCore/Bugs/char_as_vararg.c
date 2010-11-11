828d5155ce4a33384cf4c367a1b0062b0bde3660
Elnatan Reisner <elnatan@cs.umd.edu>
2010-11-11 18:04:35 +0000

    A test showing a bug in Otter stemming from CIL's not inserting casts for extra arguments to vararg functions.

---
+++ otter/test/TestOtterIntegration/OtterCore/Bugs/char_as_vararg.c
@@ -0,0 +1,20 @@
+#pragma no_other_abandoned
+
+#ifndef CIL
+#include <stdio.h>
+#define __ASSERT(x) printf((x) ? "ok\n" : "error\n")
+#endif
+
+void f(int ignore,...) {
+  __builtin_va_list args;
+  __builtin_va_start(args, ignore);
+   int x = __builtin_va_arg(args, int);
+	__ASSERT(x==1);
+  __builtin_va_end(args);
+}
+
+int main() {
+  char c = 1;
+  f(0, c); // CIL fails to cast c to an integer here, so Otter does something weird
+	return 0;
+}
