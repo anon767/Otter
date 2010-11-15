512b2b2fad293f4cb907ae516ce80ce0828a81ef
Kin-Keung Ma <kkma@cs.umd.edu>
2010-11-15 00:57:29 +0000

    Removed print_backtrace

--- otter/src/BackOtter/BackOtterDriver.ml
+++ otter/src/BackOtter/BackOtterDriver.ml
@@ -163,7 +163,6 @@ let main_loop entry_fn timer_ref interceptor queue reporter =
         (* if we got a signal, stop and return the checkpoint results *)
         Output.set_mode Output.MSG_MUSTPRINT;
         Output.printf "%s@\n" s;
-        Printexc.print_backtrace stderr;
         !checkpoint
 
 
