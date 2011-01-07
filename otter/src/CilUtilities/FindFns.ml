afb84e543a5d82ab7b87537774b0d8faccd0aca5
Kin-Keung Ma <kkma@cs.umd.edu>
2011-01-07 02:06:38 +0000

    Add --undefined-functions to output functions ever called but missing function definitions during --dofindFns mode. The "__otter_libc_" redirection is considered. But builtin functions like malloc() are still in the undefined functions list.

--- otter/src/CilUtilities/FindFns.ml
+++ otter/src/CilUtilities/FindFns.ml
@@ -1,5 +1,28 @@
 open Cil
 
+(* TODO: This is copied form Coverage.ml. Extract them and put in somewhere. *)
+class getCallerVisitor file = object (self)
+	val callee_list = ref []
+	val currFuncName = ref ""
+
+	method callee_list = !callee_list
+
+	inherit nopCilVisitor
+
+	method vinst instr = (match instr with
+      | Call(_, Lval(Var(varinfo), NoOffset) ,_,_) -> (
+          match varinfo.vtype with
+            | TFun _ ->
+                    if not (List.mem varinfo.vname (!callee_list)) then
+                        callee_list := (varinfo.vname)::(!callee_list)
+                    else ()
+            | _ -> ()
+        )
+      | _ -> ()
+    );
+		SkipChildren
+end
+
 let get_all_fnames =
     let all_fnames = ref [] in
     function file ->
@@ -13,10 +36,21 @@ let get_all_fnames =
         !all_fnames
 
 let outFile = ref ""
+let arg_undefined_functions = ref false
 
 let findFns (file : Cil.file) : unit =
 	let outChan = if !outFile = "" then stdout else open_out !outFile in
     let all_fnames = get_all_fnames file in
+    let all_fnames =
+        if !arg_undefined_functions then (
+            let vis = new getCallerVisitor file in
+            let _ = visitCilFile (vis:>cilVisitor) file in
+            let called_fnames = vis#callee_list in
+            List.filter (fun fname -> not (List.mem fname all_fnames) &&
+                                      not (List.mem ("__otter_libc_"^fname) all_fnames)) called_fnames
+        ) else all_fnames
+    in
+    let all_fnames = List.sort Pervasives.compare all_fnames in
     List.iter (fun fname -> output_string outChan (fname ^ "\n")) all_fnames;
 	close_out outChan
 
@@ -27,7 +61,10 @@ let feature : Cil.featureDescr = {
   Cil.fd_extraopt = [
 		("--fnOutfile",
 		 Arg.Set_string outFile,
-		 "<filename> The file to which to write function names (default is stdout)\n")
+         "<filename> The file to which to write function names (default is stdout)\n");
+		("--undefined-functions",
+		 Arg.Set arg_undefined_functions,
+		 " Output undefined functions instead\n")
 	];
   Cil.fd_doit = findFns;
   Cil.fd_post_check = false;
