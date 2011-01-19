cd9b6f1f24641f21fa2aac78d820300de8a12f04
Kin-Keung Ma <kkma@cs.umd.edu>
2011-01-19 15:49:02 +0000

    CIL has problems parsing these lines. Seems like they are just verifying system types and can be safely left out.

--- experiments/coreutils-8.9/lib/utimecmp.c
+++ experiments/coreutils-8.9/lib/utimecmp.c
@@ -130,8 +130,11 @@ utimecmp (char const *dst_name,
 
      time_t might be unsigned.  */
 
+  /* OTTER */
+  /*
   verify (TYPE_IS_INTEGER (time_t));
   verify (TYPE_TWOS_COMPLEMENT (int));
+  */
 
   /* Destination and source time stamps.  */
   time_t dst_s = dst_stat->st_mtime;
