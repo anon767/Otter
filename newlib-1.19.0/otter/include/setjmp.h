1da680b8043fbf4e03b716355e01a1e4fa94eeeb
Kin-Keung Ma <kkma@cs.umd.edu>
2011-01-20 20:35:58 +0000

    Now, otter's include has both setjmp.h and machine/setjmp.h, and this pair will completely shield the ones inside newlib when otter(-libc)-cc is used.
    Before that, #include<setjmp.h> would pick up newlib/libc/include/setjmp.h, which defines setjmp as a function, and at the same time includes machine/setjmp.h that turns out to be otter/include/machine/setjmp.h that defines setjmp as macro.
    
    This problem doesn't reveal when <machine/setjmp.h> is directly included, and I think that's why it's missed.

---
+++ newlib-1.19.0/otter/include/setjmp.h
@@ -0,0 +1,9 @@
+/* 
+ * Otter's setjmp.h
+ *
+ * This header, together with machine/setjmp.h, 
+ * must shield the ones inside newlib, 
+ * as these two pairs are not compatible.
+ */
+#include <machine/setjmp.h>
+
