24c3c0a5984bbe67f3c91798ad8cc4f732aec754
Jonathan Turpie <yudi@cs.umd.edu>
2011-01-25 20:47:05 +0000

    Added stdbool.h.  It seems newlib doesn't have it.

---
+++ newlib-1.19.0/otter/include/stdbool.h
@@ -0,0 +1,10 @@
+#ifndef _STDBOOL_H
+#define _STDBOOL_H
+
+/* TODO: mecro bool to _Bool when otter is updated to the new verson of cil, which supports _Bool */
+#define bool int
+#define true 1
+#define false 0
+#define __bool_true_false_are_defined 1
+
+#endif
