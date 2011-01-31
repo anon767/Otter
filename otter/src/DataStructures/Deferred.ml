8355303aa7dcf636aac656ab67497f2ab1da3e5a
Khoo Yit Phang <khooyp@cs.umd.edu>
2011-01-31 23:03:18 +0000

    Formatting and documentation.

--- otter/src/DataStructures/Deferred.ml
+++ otter/src/DataStructures/Deferred.ml
@@ -1,9 +1,11 @@
+(** Deferred computation of a value over a state. *)
 
 type ('state, 'value) t =
     | Immediate of 'value
     | Deferred of ('state -> ('state * 'value))
 
+(** Return the value of a deferred computation, computing the value in the given state if necessary. *)
 let force state = function
-	| Immediate x -> (state, x)
-	| Deferred f -> f state
+    | Immediate x -> (state, x)
+    | Deferred f -> f state
 
