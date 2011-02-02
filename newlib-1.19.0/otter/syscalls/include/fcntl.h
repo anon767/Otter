a9aaed78c8070a732478459aeff09eeb4617ff15
Kin-Keung Ma <kkma@cs.umd.edu>
2011-02-02 08:11:11 +0000

    Copy old libc's fcntl.h into syscalls/include. According to Yudi, our fcntl.h defines O_RDONLY and O_WRONLY with different values than the one in newlib/libc/include, and ours permit masking and are more preferrable.

---
+++ newlib-1.19.0/otter/syscalls/include/fcntl.h
@@ -0,0 +1,51 @@
+#ifndef _FCNTL_H
+#define _FCNTL_H
+
+#include <sys/types.h>
+
+#define F_DUPFD 1
+#define F_GETFD 2
+#define F_SETFD 3
+#define F_GETFL 4
+#define F_SETFL 5
+#define F_GETLK 6
+#define F_SETLK 7
+#define F_SETLKW 8
+#define F_GETOWN 9
+#define F_SETOWN 10
+
+#define FD_CLOEXEC 8
+
+#define F_RDLCK 1
+#define F_UNLCK 0
+#define F_WRLCK 2
+
+/* like GNU */
+#define O_ACCMODE 3
+#define O_RDONLY 1
+#define O_WRONLY 2
+#define O_RDWR 3
+
+#define O_CREAT 4
+#define O_EXCL 8
+#define O_NOCTTY 16
+#define O_TRUNC 32
+
+#define O_APPEND 64
+#define O_NONBLOCK 128
+#define O_SYNC 256
+
+struct flock
+{
+	short l_type;
+	short l_whence;
+	off_t l_start;
+	off_t l_len;
+	pid_t l_pid;
+};
+
+int creat(const char* name, mode_t mode);
+int fcntl(int fd, int cmd, ...);
+int open(const char* name, int oflag, ...);
+
+#endif
