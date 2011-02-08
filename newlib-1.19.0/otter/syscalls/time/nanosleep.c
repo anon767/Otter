0883dccc22f0132ffabfdbe111970bcdca167ca9
Elnatan Reisner <elnatan@cs.umd.edu>
2011-02-08 19:03:22 +0000

    Tweak nanosleep

--- newlib-1.19.0/otter/syscalls/time/nanosleep.c
+++ newlib-1.19.0/otter/syscalls/time/nanosleep.c
@@ -1,18 +1,16 @@
 #include <time.h>
 #include <errno.h>
 
-//Pretends that Otter runs one statment per nanosecond.
+//Pretends that Otter runs one statement per nanosecond.
 
 int nanosleep(struct timespec const *rqtp, struct timespec *rmtp)
 {
-	if(rqtp->tv_nsec < 0 || rqtp->tv_nsec > 100000)
+	if(rqtp->tv_sec != 0 || rqtp->tv_nsec < 0 || rqtp->tv_nsec > 1000000000)
 	{
 		errno = EINVAL;
 		return(-1);
 	}
-	
-	int total = rqtp->tv_nsec + rqtp->tv_sec * 1000;
-	
-	__otter_multi_time_wait(total);
+	// TODO: we should increment Otter's internal sense of the time, also.
+	__otter_multi_time_wait(rqtp->tv_nsec);
 	return(0);
 }
