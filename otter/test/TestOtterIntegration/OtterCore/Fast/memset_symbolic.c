469c351ef006364176e00c77aa5f75bb76558564
Kin-Keung Ma <kkma@cs.umd.edu>
2011-02-09 02:01:22 +0000

    Added a test for r10986.

---
+++ otter/test/TestOtterIntegration/OtterCore/Fast/memset_symbolic.c
@@ -0,0 +1,23 @@
+/* This tests makes sure that a C implementation of memset is invoked
+ * when it's given a symbolic length. */
+#pragma expect_abandoned(failure("Bounds check failed"))
+#pragma expect_return()
+#pragma expect_return()
+#pragma expect_return()
+#pragma expect_return()
+#pragma expect_return()
+#pragma expect_return()
+#pragma no_other_results
+
+char* memset(char *b, int c, int len) {
+    int i;
+    for (i=0;i<len;i++) 
+        b[i] = c;
+    return b;
+}
+
+void main() {
+    char a[5];
+    int n; __SYMBOLIC(&n);
+    memset(a,0,n);
+}
