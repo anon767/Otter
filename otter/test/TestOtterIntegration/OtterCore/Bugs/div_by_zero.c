edfc30e46dae37bc5511cd27cd6c2e38a1008851
Elnatan Reisner <elnatan@cs.umd.edu>
2011-02-10 18:37:07 +0000

    Otter doesn't check for division by zero, and apparently STP does something weird in such cases.

---
+++ otter/test/TestOtterIntegration/OtterCore/Bugs/div_by_zero.c
@@ -0,0 +1,12 @@
+/* Test Otter's handling of division by zero. There should one error path (x==0)
+	 and only one other path, because 0/x is 0 if x is nonzero. */
+#pragma expect_abandoned(failure(""))
+#pragma expect_return(__return_code__ == 1)
+#pragma no_other_results
+
+int main() {
+	int x;
+	__SYMBOLIC(&x);
+	if (0/x) return 0;
+	return 1;
+}
