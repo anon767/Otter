d5c9e5d5309260269f91cd860360ddfb43a01ae0
Elnatan Reisner <elnatan@cs.umd.edu>
2011-02-10 19:15:35 +0000

    Replace the select.h that Martin grabbed with a modified version of one that came from newlib itself. Among other things, FD_SET and friends are already declared in newlib, although they are (for some unknown reason) defined in sys/types.h.

--- newlib-1.19.0/otter/include/sys/select.h
+++ newlib-1.19.0/otter/include/sys/select.h
@@ -1,32 +1,12 @@
-/* Copied from http://www.koders.com/c/fid53A7AC4B5D3463A7C0A7FF6E837BA26E9AFB4316.aspx?s=cdefs.h */
-
 #ifndef _SYS_SELECT_H
-#define _SYS_SELECT_H
+#define _SYS_SELECT_H	1
 
-#include <string.h>
+#include <sys/types.h>
+#include <sys/time.h>
 
-typedef long int fd_mask;
+int select (int __nfds, fd_set *__readfds,
+		   fd_set *__writefds,
+		   fd_set *__exceptfds,
+		   struct timeval *__timeout);
 
-#define NFDBITS	(8 * sizeof(unsigned long))
-#define FD_SETSIZE	1024
-#define __FDSET_LONGS	(FD_SETSIZE/NFDBITS)
-#define __FDELT(d)	((d) / NFDBITS)
-#define __FDMASK(d)	(1UL << ((d) % NFDBITS))
-
-typedef struct {
-  unsigned long fds_bits [__FDSET_LONGS];
-} fd_set;
-
-#define FD_SET(d, set)	((set)->fds_bits[__FDELT(d)] |= __FDMASK(d))
-#define FD_CLR(d, set)	((set)->fds_bits[__FDELT(d)] &= ~__FDMASK(d))
-#define FD_ISSET(d, set)	(((set)->fds_bits[__FDELT(d)] & __FDMASK(d)) != 0)
-#define FD_ZERO(set)	\
-  ((void) memset ((void*) (set), 0, sizeof (fd_set)))
-
-int select(int nfds, 
-           fd_set *restrict readfds, 
-           fd_set *restrict writefds, 
-           fd_set *restrict errorfds, 
-           struct timeval *restrict timeout);
-
-#endif
+#endif /* sys/select.h */
