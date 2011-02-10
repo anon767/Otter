fa57e01946fa7e8b5e152480419f7b16e0f570c9
Kin-Keung Ma <kkma@cs.umd.edu>
2011-02-10 03:48:38 +0000

    Forgot to declare strto{i,u}max in inttypes.h.
    This would make coreutils enable its own declarations of the functions, which are not compatible to ours.

--- newlib-1.19.0/otter/include/inttypes.h
+++ newlib-1.19.0/otter/include/inttypes.h
@@ -3,4 +3,7 @@
 
 #include <stdint.h>
 
+intmax_t  strtoimax(const char * restrict nptr, char ** restrict endptr, int base);
+uintmax_t strtoumax(const char * restrict nptr, char ** restrict endptr, int base);
+
 #endif
