2564cfe8cd31ea1a40ae6718dee88ae87eec9f03
Elnatan Reisner <elnatan@cs.umd.edu>
2011-02-15 21:06:29 +0000

    Clean up some tests a bit

--- otter/test/TestOtterIntegration/OtterCore/Bugs/copy_pointer0.c
+++ otter/test/TestOtterIntegration/OtterCore/Bugs/copy_pointer0.c
@@ -5,13 +5,11 @@ struct s { char *p; };
 
 int main() {
 	struct s s1 = {"hello"}, *s2 = malloc(sizeof(s1));
-	char *ps1 = &s1, *ps2 = (char*)s2;
+	char *ps1 = (char*)&s1, *ps2 = (char*)s2;
 	int n = sizeof(s1);
 	while (n--) {
 		*ps2++ = *ps1++;
 	}
-	__EVAL(s1);
-	__EVAL(*s2);
 	__ASSERT(s2->p[0] == 'h');
 	return 0;
 }
--- otter/test/TestOtterIntegration/OtterCore/Fast/copy_pointer.c
+++ otter/test/TestOtterIntegration/OtterCore/Fast/copy_pointer.c
@@ -19,8 +19,6 @@ struct s { char *p; };
 int main() {
 	struct s s1 = {"hello"}, *s2 = malloc(sizeof(s1));
 	memcpy(s2, &s1, sizeof(s1));
-	__EVAL(s1);
-	__EVAL(*s2);
 	__ASSERT(s2->p[0] == 'h');
 	return 0;
 }
