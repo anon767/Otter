995e46102a2199c7e45a7b6554f74ca0facbf68b
Kin-Keung Ma <kkma@cs.umd.edu>
2011-02-15 00:29:03 +0000

    Reimplemented TimedSet, which fixes the problem of wrong reporting of times when elements are added to sets.
    This change requires features in the Map module in ocaml 3.12.

--- otter/src/DataStructures/TimedSet.ml
+++ otter/src/DataStructures/TimedSet.ml
@@ -1,15 +1,50 @@
+(** 
+ * A set module that keeps track of times when elements are added.
+ * The underlying data-structure is a map from the set elements to times as floats.
+ *
+ * For intersection/union, when an element appears in both sets, the earlier time is used.
+ *)
 module Make (Ord: Set.OrderedType) = struct
+    
     let time_elapsed =
         let time_start = Unix.gettimeofday () in
         function () -> Unix.gettimeofday () -. time_start
 
-    module TimedOrd = struct
-        type t = Ord.t * float
-        let compare (e1, _) (e2, _) = Ord.compare e1 e2
-    end
-    module S = Set.Make (TimedOrd)
-    include S
+    module M = Map.Make (Ord)
 
-    let add elt s = S.add (elt, time_elapsed ()) s
-    let iter f s = S.iter (fun (elt, _) -> f elt) s
+    type t = float M.t
+
+    let empty = M.empty
+    let is_empty = M.is_empty
+    let cardinal = M.cardinal
+    let iter f s = M.iter (fun elt _ -> f elt) s
+    let elements s = M.fold (fun elt t lst -> (elt, t)::lst) s []
+
+    let add elt s = 
+        let t = time_elapsed () in
+        try
+            let t' = M.find elt s in
+            if t' <= t then s else M.add elt t s
+        with Not_found -> M.add elt t s
+
+    let union s1 s2 = M.merge (
+        fun _ t1opt t2opt -> match t1opt, t2opt with
+        | Some t, None
+        | None, Some t -> Some t
+        | Some t1, Some t2 -> Some (Pervasives.min t1 t2)
+        | None, None -> None (* Can't happen *)
+    ) s1 s2
+
+    let inter s1 s2 = M.merge (
+        fun _ t1opt t2opt -> match t1opt, t2opt with
+        | Some t1, Some t2 -> Some (Pervasives.min t1 t2)
+        | _, _ -> None 
+    ) s1 s2
+
+    let diff s1 s2 =  M.merge (
+        fun _ t1opt t2opt -> match t1opt, t2opt with
+        | Some t, None -> Some t
+        | _, _ -> None 
+    ) s1 s2
+
 end
