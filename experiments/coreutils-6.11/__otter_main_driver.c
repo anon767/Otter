a5b7926a9a3332c9d272658834407204ca802e76
Kin-Keung Ma <kkma@cs.umd.edu>
2011-02-19 21:18:04 +0000

    Improvement of initialization

--- experiments/coreutils-6.11/__otter_main_driver.c
+++ experiments/coreutils-6.11/__otter_main_driver.c
@@ -1,11 +1,14 @@
+#include "otter/otter_fs.h"
+#include "otter/otter_user.h"
+#include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <fcntl.h>
 
 // The current setting is the same as KLEE
 #define PROGRAM_NAME     "program"
-#define MAX_ARG          3             // Excluding argv[0]===PROGRAM_NAME
-#define MAX_ARG_LENGTHS  {8, 10, 2, 2} 
+#define MAX_ARGC         4
+#define MAX_ARG_LENGTHS  {8, 2, 2, 10} 
 #define MAX_FILE         2
 #define MAX_FILE_SIZE    8             // TODO: this should also control the size of stdin
 #define MAX_FILENAME_LENGTH   5
@@ -38,29 +41,39 @@ char* symbolic_string(int len) {
 
 #pragma cilnoremove("__otter_main_driver")
 int __otter_main_driver() {
-    int argc;
-    char* argv[MAX_ARG+1];
     int i;
+    int argc;
+    char* argv[MAX_ARGC+1];  // One for null-termination
     
     // Set up argc
-    __SYMBOLIC(&argc);
-    __ASSUME(1<=argc);
-    __ASSUME(argc<=MAX_ARG);
+    // TODO: symbolic argc
+    // __SYMBOLIC(&argc);
+    // __ASSUME(1<=argc);
+    // __ASSUME(argc<=MAX_ARGC);
+    argc = MAX_ARGC;
 
     // Set up argv
     int arg_lengths[] = MAX_ARG_LENGTHS;
-    for (i=0;i<=MAX_ARG;i++) {
+    for (i=0;i<MAX_ARGC;i++) {
         argv[i] = symbolic_string(arg_lengths[i]);
     }
+    argv[MAX_ARGC] = 0;
 
     // Set up stdin, stdout and stderr
 	__otter_fs_mount();
-	int stdin  = fopen("/dev/tty", "r"); // assert: fopen returns 0  
-	int stdout = fopen("/dev/tty", "w"); // assert: fopen returns 1   
-	int stderr = fopen("/dev/tty", "w"); // assert: fopen returns 2 
+    setuid(__otter_UID_ROOT);
+	stdin  = fopen("/dev/tty", "r"); // assert: fopen returns 0  
+	stdout = fopen("/dev/tty", "w"); // assert: fopen returns 1   
+	stderr = fopen("/dev/tty", "w"); // assert: fopen returns 2 
 
-    // TODO: Set up the file system
+    // Set up the file system
+    struct __otter_fs_dnode* dnode = __otter_fs_mkdir("etc", __otter_fs_root);
+    __otter_fs_touch("/etc/file", dnode);
+    FILE* f = fopen("/etc/file", "w");
+    fprintf(f,"Hello world");
+    fclose(f);
     
+    // Set up env
     for (i=0;i<MAX_ENVIRON;i++)
         __otter_environ[i] = symbolic_string(MAX_ENVVAR_LENGTH + 1 + MAX_ENVVAL_LENGTH);  // "name=value"
     __otter_environ[MAX_ENVIRON] = 0;
