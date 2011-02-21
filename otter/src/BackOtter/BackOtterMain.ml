b0c715e85e1df168a524f0b2dc0babaf479cf833
Kin-Keung Ma <kkma@cs.umd.edu>
2011-02-21 20:51:41 +0000

    Reverted part of the change in r11211; options actually accumulate in runotter.ml.

--- otter/src/BackOtter/BackOtterMain.ml
+++ otter/src/BackOtter/BackOtterMain.ml
@@ -191,7 +191,6 @@ let options = [
         Arg.Set_int default_conditionals_forking_limit,
         "<limit> Set the limit in conditionals forking (default: max_int (== don't use))";
 ] @
-    Otter.Executemain.options @
     BackOtterInterceptor.options @ 
     BackOtterReporter.options @ 
     BackOtterTimer.options @ 
