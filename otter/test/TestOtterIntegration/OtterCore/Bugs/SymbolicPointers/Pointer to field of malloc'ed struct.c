284e28148cf1537b462ebbe923a1d81adacc6580
Khoo Yit Phang <khooyp@cs.umd.edu>
2011-02-21 22:39:12 +0000

    Correct a type error (x should point to y->b), and explain the test expectation in more detail.

--- otter/test/TestOtterIntegration/OtterCore/Bugs/SymbolicPointers/Pointer to field of malloc'ed struct.c
+++ otter/test/TestOtterIntegration/OtterCore/Bugs/SymbolicPointers/Pointer to field of malloc'ed struct.c
@@ -3,12 +3,16 @@
 #pragma expect_return(__return_code__ == 1) /* for the assignment to the non-null leaf node */
 #pragma no_other_abandoned
 
-/* This tests that malloc'ed structs reached via a pointer to a field are initialized correctly. */
+/* This tests that malloc'ed structs, which contain pointers, that are reached via a pointer to a field are initialized
+   correctly. Here, x points to y->f, where y is a struct s. When x is dereferenced in foo, a fresh struct s
+   should be initialized (as an alias of y), not a fresh int (i.e., the type pointed to by x), and x initialized to
+   point to field f of that struct.
+*/
 
 int *x;
 struct s {
     int *a;
-    int b;
+    int f;
 };
 int foo(void) {
     if (x) {
@@ -19,6 +23,6 @@ int foo(void) {
 }
 int main(void) {
     struct s * y = malloc(sizeof(struct s));
-    x = &y->a;
+    x = &y->f;
     return foo();
 }
