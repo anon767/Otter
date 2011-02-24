5148552f33f9b4bf5a493f1fbe7c2bdf839c0254
Elnatan Reisner <elnatan@cs.umd.edu>
2011-02-24 21:34:11 +0000

    Removing duplicate code

--- otter/src/CilUtilities/WidenBitfields.ml
+++ otter/src/CilUtilities/WidenBitfields.ml
@@ -7,12 +7,3 @@ let apply file =
              | Cil.GCompTag (compinfo, _) ->
                    List.iter (fun fieldinfo -> fieldinfo.Cil.fbitfield <- None) compinfo.Cil.cfields
              | _ -> ())
-(** Widen all bit-fields in a {!Cil.file} into standard fields *)
-
-let apply file =
-    Cil.iterGlobals
-        file
-        (function
-             | Cil.GCompTag (compinfo, _) ->
-                   List.iter (fun fieldinfo -> fieldinfo.Cil.fbitfield <- None) compinfo.Cil.cfields
-             | _ -> ())
