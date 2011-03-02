c7873d709d817b2dbbd5457518d5c7ae4c77a6b5
Elnatan Reisner <elnatan@cs.umd.edu>
2011-03-02 04:43:54 +0000

    Make the run-client.sh track coverage during execution, and make the otter-make script generate the list of functions we want to track (that is, the list of all functions in redis-server)

--- experiments/redis-2.2.0-rc4/run-client.sh
+++ experiments/redis-2.2.0-rc4/run-client.sh
@@ -1,5 +1,10 @@
-if [ -z "$*" ]; then
+if [ -z "$@" ]; then
   echo "Usage: ./run-client.sh <client program> [options...]"
   exit
 fi
-../../newlib-1.19.0/otter/otter-with-libc src/redis-server_comb.c deps/hiredis/libhiredis.a --domultiotter --doRunRmtmps $*
+../../newlib-1.19.0/otter/otter-with-libc\
+  src/redis-server_comb.c deps/hiredis/libhiredis.a\
+  --domultiotter --doRunRmtmps\
+  --lineCov --pathCov --edgeCov --blockCov\
+  --tracked-functions=redis-functions\
+  "$@"
