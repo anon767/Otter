71997518775f1e99dcbe903c2e55558bfd711e7f
Kin-Keung Ma <kkma@cs.umd.edu>
2011-03-02 15:27:43 +0000

    Fixed a bug of using job#node_id to distinguish jobs: node_id's are not unique!

--- otter/src/BackOtter/ClosestToTargetsStrategy.ml
+++ otter/src/BackOtter/ClosestToTargetsStrategy.ml
@@ -16,12 +16,12 @@ let get_distance_to_targets =
         type t = (CilData.CilFundec.t list) * (Instruction.t list) * Job.t
         (* TODO: factor out the list comparison and hasing *)
         let equal (f1, l1, j1) (f2, l2, j2) = 
-            j1#node_id = j2#node_id &&
+            j1 == j2 &&
             List.length f1 = List.length f2 && 
             List.fold_left2 (fun b i1 i2 -> CilData.CilFundec.equal i1 i2 && b) true f1 f2 &&
             List.length l1 = List.length l2 && 
             List.fold_left2 (fun b i1 i2 -> Instruction.equal i1 i2 && b) true l1 l2 
-        let hash (f, l, j) = List.fold_left (fun h i -> h + CilData.CilFundec.hash i) 0 f + List.fold_left (fun h i -> h + Instruction.hash i) 0 l + j#node_id
+        let hash (f, l, j) = List.fold_left (fun h i -> h + CilData.CilFundec.hash i) 0 f + List.fold_left (fun h i -> h + Instruction.hash i) 0 l + j#jid_unique
     end) in
     Memo.memo "ClosestToTargetsStrategy.get_distance_to_targets" (fun (target_fundecs, line_targets, job) ->
         Profiler.global#call "ClosestToTargetsStrategy.get_distance_to_targets" begin fun () ->
@@ -45,6 +45,7 @@ class ['self] t = object (self : 'self)
         let target_fundecs = BackOtterTargets.get_target_fundecs () in
         let line_targets = LineTargets.get_line_targets job#file in
         let distance = get_distance_to_targets (target_fundecs, line_targets, job) in
+        Output.debug_printf "Job %d has distance to target = %d@\n" job#node_id distance;
         1. /. float_of_int (distance)
 end
 
