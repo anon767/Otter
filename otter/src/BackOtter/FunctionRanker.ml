592cd834731fba6c097ac8cfe7df47d03e82ff95
Kin-Keung Ma <kkma@cs.umd.edu>
2011-03-02 04:13:37 +0000

    Added a round robin function ranking strategy. Among a set of functions, it picks the one that has been idled for the longest (new function is the oldest).

--- otter/src/BackOtter/FunctionRanker.ml
+++ otter/src/BackOtter/FunctionRanker.ml
@@ -3,26 +3,35 @@ open OcamlUtilities
 open OtterCore
 
 let ranker eval lst =
-    (* TODO: doesn't need to recalculate every time (although this is not bottleneck *)
     let lst = List.map (fun (file, fundec) -> (fundec, eval (file, fundec))) lst in
-    let lst = List.sort (fun (f1, r1) (f2, r2) -> (Pervasives.compare r2 r1)) lst in
+    let lst = List.sort (fun (f1, r1) (f2, r2) -> (Pervasives.compare r1 r2)) lst in
     List.map (fun (f, _) -> f) lst
 
-(* FIXME: this can be inefficient if the function is unreachable from entryfn *)
 let distance_from_entryfn (file, fundec) =
     let entry_fn = ProgramPoints.get_entry_fundec file in
     let distance = CilCallgraph.get_distance file entry_fn fundec in
-    -. (float_of_int distance)
+    float_of_int distance
 
 (* TODO: this is not compatible with line-targets *)
 let distance_from_failurefn (file, fundec) =
     let failure_fn = ProgramPoints.get_failure_fundec file in
     let distance = CilCallgraph.get_distance file fundec failure_fn in
-    -. (float_of_int distance)
+    float_of_int distance
 
 let random_function _ = Random.float 1.0
 
-let total rank_fn = rank_fn
+let round_robin_ranker =
+    let counter = ref 0 in
+    let module FundecMap = Map.Make (CilData.CilFundec) in
+    let history = ref FundecMap.empty in
+    fun lst ->
+        let fundecs = ranker (fun (_, fundec) -> try FundecMap.find fundec (!history) with Not_found -> -1 (* fresh *)) lst in
+        assert(fundecs <> []);
+        history := FundecMap.add (List.hd fundecs) (!counter) (!history); 
+        counter := (!counter) + 1;
+        fundecs
+
+
 let partial rank_fn = if Random.bool () then rank_fn else (ranker random_function)
 
 let queues = [
@@ -30,13 +39,15 @@ let queues = [
     "closest-to-failure", `ClosestToFailure;
     "partial*closest-to-entry", `Partial `ClosestToEntry;
     "random-function", `RandomFunction;
+    "round-robin", `RoundRobin;
 ]
 
 let get = function
-    | `ClosestToEntry -> total (ranker distance_from_entryfn)
+    | `ClosestToEntry -> ranker distance_from_entryfn
     | `Partial `ClosestToEntry -> partial (ranker distance_from_entryfn)
-    | `ClosestToFailure -> total (ranker distance_from_failurefn)
-    | `RandomFunction -> total (ranker random_function)
+    | `ClosestToFailure -> ranker distance_from_failurefn
+    | `RandomFunction -> ranker random_function
+    | `RoundRobin -> round_robin_ranker
 
 let default_brank = ref `ClosestToEntry
 
