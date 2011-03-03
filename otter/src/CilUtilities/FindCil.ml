29d84deba17fd4d6586c9e29325f5df2fac28a56
Khoo Yit Phang <khooyp@cs.umd.edu>
2011-03-03 20:54:48 +0000

    Add FindCil.stmts_by_line to find all stmts at a given file name and line number.

--- otter/src/CilUtilities/FindCil.ml
+++ otter/src/CilUtilities/FindCil.ml
@@ -17,11 +17,13 @@ let searchtables =
             let varinfo_to_varinit = Hashtbl.create 100 in
             let name_to_fundec = Hashtbl.create 100 in
             let name_to_global_varinfo = Hashtbl.create 100 in
+            let line_to_stmts = Hashtbl.create 100 in
             let all_varinfos = ref VarinfoSet.empty in
             let all_types = ref TypeSet.empty in
 
             Cil.visitCilFile begin object
                 inherit Cil.nopCilVisitor
+                val mutable current_fundec = Cil.dummyFunDec
                 method vtype typ =
                     all_types := TypeSet.add typ !all_types;
                     Cil.DoChildren
@@ -31,9 +33,16 @@ let searchtables =
                     all_varinfos := VarinfoSet.add varinfo !all_varinfos;
                     Cil.DoChildren
                 method vfunc fundec =
+                    current_fundec <- fundec;
                     Hashtbl.replace name_to_fundec fundec.Cil.svar.Cil.vname fundec;
                     List.iter (fun varinfo -> all_varinfos := VarinfoSet.add varinfo !all_varinfos) fundec.Cil.sformals;
                     List.iter (fun varinfo -> all_varinfos := VarinfoSet.add varinfo !all_varinfos) fundec.Cil.slocals;
+                    Cil.DoChildren
+                method vstmt stmt =
+                    let loc = Cil.get_stmtLoc stmt.Cil.skind in
+                    let line = (loc.Cil.file, loc.Cil.line) in
+                    let stmts = try Hashtbl.find line_to_stmts line with Not_found -> [] in
+                    Hashtbl.replace line_to_stmts line ((current_fundec, stmt)::stmts);
                     Cil.DoChildren
                 method vinit varinfo _ init =
                     Hashtbl.replace varinfo_to_varinit varinfo { Cil.init = Some init };
@@ -47,6 +56,7 @@ let searchtables =
                 method varinfo_to_varinit = Hashtbl.find varinfo_to_varinit
                 method name_to_fundec = Hashtbl.find name_to_fundec
                 method name_to_global_varinfo = Hashtbl.find name_to_global_varinfo
+                method line_to_stmts = Hashtbl.find line_to_stmts
                 method all_varinfos = all_varinfos
                 method all_types = all_types
             end
@@ -90,6 +100,16 @@ let fundec_by_name file name =
 *)
 let global_varinfo_by_name file name =
     (searchtables file)#name_to_global_varinfo name
+
+
+(** Find a list of statements by file name and line number from a {!Cil.file}.
+        @param file the {!Cil.file} to find the {!Cil.varinfo} in
+        @param line the line to find the statements, as a [(filename, line)] pair
+        @return the list of statements as {!Cil.fundec}-by-{!Cil.stmt} pairs
+        @raise Not_found if no statements can be found at [line] in [file]
+*)
+let stmts_by_line file line =
+    (searchtables file)#line_to_stmts line
 
 
 (** Return a list of all {!Cil.varinfo} in a {!Cil.file}.
