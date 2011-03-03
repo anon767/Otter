ab8d2c5961a72cdd7622f6690ef1afb248efb3e3
Khoo Yit Phang <khooyp@cs.umd.edu>
2011-03-03 21:05:26 +0000

    Switch BackOtter.ClosestToTargetsStrategy to use ListPlus.MakeHashedList.

--- otter/src/BackOtter/ClosestToTargetsStrategy.ml
+++ otter/src/BackOtter/ClosestToTargetsStrategy.ml
@@ -10,17 +10,13 @@ open OtterCore
 let get_distance_to_targets =
     let max_distance = max_int in
     let module Memo = Memo.Make (struct
-        type t = (CilData.CilFundec.t list) * (Instruction.t list) * Job.t
-        (* TODO: factor out the list comparison and hasing *)
-        let equal (f1, l1, j1) (f2, l2, j2) = 
-            j1 == j2 &&
-            List.length f1 = List.length f2 && 
-            List.fold_left2 (fun b i1 i2 -> CilData.CilFundec.equal i1 i2 && b) true f1 f2 &&
-            List.length l1 = List.length l2 && 
-            List.fold_left2 (fun b i1 i2 -> Instruction.equal i1 i2 && b) true l1 l2 
-        let hash (f, l, j) = List.fold_left (fun h i -> h + CilData.CilFundec.hash i) 0 f + List.fold_left (fun h i -> h + Instruction.hash i) 0 l + j#jid_unique
+        module F = ListPlus.MakeHashedList (CilData.CilFundec)
+        module I = ListPlus.MakeHashedList (Instruction)
+        type t = Job.t * CilData.CilFundec.t list * Instruction.t list
+        let equal (j1, f1, i1) (j2, f2, i2) = j1 == j2 && F.equal f1 f2 && I.equal i1 i2
+        let hash (j, f, i) = Hashtbl.hash (j#jid_unique, F.hash f, I.hash i)
     end) in
-    Memo.memo "ClosestToTargetsStrategy.get_distance_to_targets" (fun (target_fundecs, line_targets, job) ->
+    Memo.memo "ClosestToTargetsStrategy.get_distance_to_targets" (fun (job, target_fundecs, line_targets) ->
         Profiler.global#call "ClosestToTargetsStrategy.get_distance_to_targets" begin fun () ->
             let source = Job.get_instruction job in
             let current_fundec = List.hd job#state.State.callstack in
@@ -42,7 +38,7 @@ class ['self] t = object (self : 'self)
     method weight job =
         let target_fundecs = BackOtterTargets.get_target_fundecs () in
         let line_targets = LineTargets.get_line_targets job#file in
-        let distance = get_distance_to_targets (target_fundecs, line_targets, job) in
+        let distance = get_distance_to_targets (job, target_fundecs, line_targets) in
         Output.debug_printf "Job %d has distance to target = %d@\n" job#node_id distance;
         1. /. float_of_int (distance)
 end
