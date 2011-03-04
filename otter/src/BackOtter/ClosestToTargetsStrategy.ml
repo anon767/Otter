71cfbd252bed1f7d7668da5db6cc7f3b0620ea1b
Kin-Keung Ma <kkma@cs.umd.edu>
2011-03-04 02:21:52 +0000

    Extracted the get_distance_to_targets procedure out from weight. This can be used to suppress a job from running, if it has infinite distance to targets.

--- otter/src/BackOtter/ClosestToTargetsStrategy.ml
+++ otter/src/BackOtter/ClosestToTargetsStrategy.ml
@@ -7,7 +7,7 @@ open OtterCFG
 open OtterCore
 
 
-let get_distance_to_targets =
+let get_distance_to_targets_impl =
     let max_distance = max_int in
     let module Memo = Memo.Make (struct
         module F = ListPlus.MakeHashedList (CilData.CilFundec)
@@ -16,8 +16,8 @@ let get_distance_to_targets =
         let equal (j1, f1, i1) (j2, f2, i2) = j1 == j2 && F.equal f1 f2 && I.equal i1 i2
         let hash (j, f, i) = Hashtbl.hash (j#jid_unique, F.hash f, I.hash i)
     end) in
-    Memo.memo "ClosestToTargetsStrategy.get_distance_to_targets" (fun (job, target_fundecs, line_targets) ->
-        Profiler.global#call "ClosestToTargetsStrategy.get_distance_to_targets" begin fun () ->
+    Memo.memo "ClosestToTargetsStrategy.get_distance_to_targets_impl" 
+        (fun (job, target_fundecs, line_targets) ->
             let source = Job.get_instruction job in
             let current_fundec = List.hd job#state.State.callstack in
             let target_instrs = List.fold_left (fun lst f -> (Instruction.call_sites_in_caller job#file current_fundec f) @ lst) [] target_fundecs in
@@ -27,7 +27,16 @@ let get_distance_to_targets =
             else
                 let context = Job.get_instruction_context job in
                 Distance.find_in_context (source, context, all_targets)
-        end)
+        )
+
+let get_distance_to_targets job =
+    Profiler.global#call "ClosestToTargetsStrategy.get_distance_to_targets" begin fun () ->
+        let target_fundecs = BackOtterTargets.get_target_fundecs () in
+        let line_targets = LineTargets.get_line_targets job#file in
+        let distance = get_distance_to_targets_impl (job, target_fundecs, line_targets) in
+        Output.debug_printf "Job %d has distance to target = %d@\n" job#node_id distance;
+        distance
+    end
 
 class ['self] t = object (self : 'self)
 
@@ -36,10 +45,7 @@ class ['self] t = object (self : 'self)
     method remove job = self
 
     method weight job =
-        let target_fundecs = BackOtterTargets.get_target_fundecs () in
-        let line_targets = LineTargets.get_line_targets job#file in
-        let distance = get_distance_to_targets (job, target_fundecs, line_targets) in
-        Output.debug_printf "Job %d has distance to target = %d@\n" job#node_id distance;
+        let distance = get_distance_to_targets job in 
         1. /. float_of_int (distance)
 end
 
