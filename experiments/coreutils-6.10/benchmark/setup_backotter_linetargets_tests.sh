6a4869b3e15b07147bdaab31dd1b8942daabafd6
Kin-Keung Ma <kkma@cs.umd.edu>
2011-03-05 22:15:47 +0000

    Various files needed to setup ESD-style runs.

--- experiments/coreutils-6.10/benchmark/setup_backotter_linetargets_tests.sh
+++ experiments/coreutils-6.10/benchmark/setup_backotter_linetargets_tests.sh
@@ -1,19 +1,20 @@
 #!/bin/sh
 
-if [ $# -ne 5 ]
+if [ $# -ne 6 ]
 then
-    echo "Usage: ./setup_backotter_tests.sh <base-directory> <experiment-name> <programs_dir> <programs.in> <options.in>"
+    echo "Usage: ./setup_backotter_tests.sh <base-directory> <line_targets_dir> <experiment-name> <programs_dir> <programs.in> <options.in>"
     exit 1
 fi
 
 base="$1"
-exp_name="$(echo $2|sed 's/ /_/g')"
+line_targets_dir="$2"
+exp_name="$(echo $3|sed 's/ /_/g')"
 exp_base="$base/backotter_$(date "+%Y_%m_%d_%H")_$exp_name"
 runbackotter="$(pwd)/runbackotter"
 
-programs_dir="$3"
-programs_in="$4"
-options_in="$5"
+programs_dir="$4"
+programs_in="$5"
+options_in="$6"
 
 mkdir -p "$exp_base"
 chmod 755 "$exp_base"
@@ -29,8 +30,9 @@ do
         opts_name=$(echo $options|sed 's/ /_/g')
         log_file="$exp_base/results/$prog_name/run_with_$opts_name.log"
         test_sh="$exp_base/tests/${prog_name}_run_with_$opts_name.sh"
+        line_targets_file="$line_targets_dir/$prog_name.line_targets"
         mkdir -p "$(dirname "$test_sh")"
-        echo "mkdir -p \"$(dirname "$log_file")\" && \"$runbackotter\" \"$prog\" $options 2>&1 | timelines > \"$log_file\"" >> $test_sh
+        echo "mkdir -p \"$(dirname "$log_file")\" && \"$runbackotter\" \"$prog\" --line-targets-file=$line_targets_file $options 2>&1 | timelines > \"$log_file\"" >> $test_sh
     done
 done
 
