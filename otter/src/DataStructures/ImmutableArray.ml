67ee17b7f1a53ce17873258c2fb1d5015c57283b
Khoo Yit Phang <khooyp@cs.umd.edu>
2011-03-06 22:55:11 +0000

    Refactor ImmutableArray.equal to use a much less complicated way to take advantage of sparsity.
    Also, just to mention, I accidentally checked in ImmutableArray.hash in r11548, but it should work fine.

--- otter/src/DataStructures/ImmutableArray.ml
+++ otter/src/DataStructures/ImmutableArray.ml
@@ -78,14 +78,13 @@ let of_list (lst: 'a list) : 'a t =
 
 
 let equal eq xs ys =
-    (* the use of default values makes this tremendously complicated *)
-    let module E = struct exception Not_equal exception Reached_end end in
-    let default_equal () =
-        match xs.default, ys.default with
-            | Some x, Some y -> eq x y
-            | _ -> false
-    in
+    let module E = struct exception Not_equal end in
     let shallow_equal () =
+        let default_equal () =
+            match xs.default, ys.default with
+                | Some x, Some y -> eq x y
+                | _ -> false
+        in
         xs.offset = ys.offset
         && xs.map == ys.map
         (* defaults are equal or unused *)
@@ -93,58 +92,20 @@ let equal eq xs ys =
     in
     let deep_equal () =
         try
-            (* compare xs.map bindings in ys.map or against ys.default *)
-            let xcount = ref 0 in
-            let ymap = ref ys.map in
-            let ycount = ref 0 in
-            begin try
-                IndexMap.iter begin fun xk xv ->
-                    if xk >= xs.offset + xs.length then
-                        raise E.Reached_end
-                    else if xk >= xs.offset then
-                        try
-                            let yk = xk - xs.offset + ys.offset in
-                            let yv = IndexMap.find yk !ymap in
-                            if eq xv yv then begin
-                                incr xcount;
-                                ymap := IndexMap.remove yk !ymap;
-                                incr ycount;
-                            end else
-                                raise E.Not_equal
-                        with Not_found ->
-                            match ys.default with
-                                | Some yv when eq xv yv -> incr xcount
-                                | _ -> raise E.Not_equal
-                end xs.map
-            with E.Reached_end ->
-                ()
-            end;
+            let length = xs.length in
+            if length > 0 then
+                for xk = xs.offset to xs.offset + xs.length - 1 do
+                    let xv_opt = try Some (IndexMap.find xk xs.map) with Not_found -> xs.default in
 
-            if IndexMap.is_empty !ymap then
-                (* defaults are equal or unused *)
-                xs.default == ys.default || !xcount = xs.length || default_equal ()
-            else
-                (* compare the remainder of ys.map against xs.default *)
-                begin match xs.default with
-                    | Some xv ->
-                        begin try
-                            IndexMap.iter begin fun yk yv ->
-                                if yk >= ys.offset + ys.length then
-                                    raise E.Reached_end
-                                else if yk >= ys.offset then
-                                    if eq xv yv then
-                                        incr ycount
-                                    else
-                                        raise E.Not_equal
-                            end !ymap
-                        with E.Reached_end ->
-                            ()
-                        end;
-                        (* defaults are equal or unused *)
-                        xs.default == ys.default || !ycount = ys.length || default_equal ()
-                    | None ->
-                        false
-                end
+                    let yk = xk - xs.offset + ys.offset in
+                    let yv_opt = try Some (IndexMap.find yk ys.map) with Not_found -> ys.default in
+
+                    match xv_opt, yv_opt with
+                        | Some xv, Some yv when eq xv yv -> ()
+                        | None, None -> ()
+                        | _, _ -> raise E.Not_equal
+                done;
+            true
         with E.Not_equal ->
             false
     in
