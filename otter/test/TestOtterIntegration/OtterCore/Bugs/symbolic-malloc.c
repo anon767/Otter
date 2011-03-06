718b49e23b89ffcc949e1e03342a2f40b554fb99
Elnatan Reisner <elnatan@cs.umd.edu>
2011-03-06 21:00:23 +0000

    A test showing that Otter fails to handle symbolic arguments to malloc properly. This fails because of bounds-checking, because Otter currently mallocs a block of size 1 if the argument is symbolic. This worked before bounds-checking existed, and it probably still works if bounds-checking is turned off.

---
+++ otter/test/TestOtterIntegration/OtterCore/Bugs/symbolic-malloc.c
@@ -0,0 +1,12 @@
+/* Test Otter's handling of symbolically sized mallocs. */
+#pragma expect_return()
+#pragma no_other_results
+
+int main() {
+    unsigned int x;
+    __SYMBOLIC(&x);
+    __ASSUME(x == 2);
+    int *p = malloc(x);
+    p[1] = 0;
+    return 0;
+}
