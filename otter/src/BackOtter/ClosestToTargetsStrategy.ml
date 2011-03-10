83bdb2450df4004ad4a5912cb9b1c93e6f51f009
Khoo Yit Phang <khooyp@cs.umd.edu>
2011-03-10 23:44:37 +0000

    Overhauled BackOtter.ClosestToTargetsStrategy: - do not use Job.t as a key to Memo.Make, that's a big memory leak since jobs cannot be GC'ed; - instead, calculate distances as jobs are put into the queue; - remove dependence on jid_unique (which will soon be removed).

--- otter/src/BackOtter/ClosestToTargetsStrategy.ml
+++ otter/src/BackOtter/ClosestToTargetsStrategy.ml
@@ -2,52 +2,51 @@
 
 open OcamlUtilities
 open CilUtilities
-open DataStructures
 open OtterCFG
 open OtterCore
 
 
-let get_distance_to_targets_impl =
-    let max_distance = max_int in
-    let module Memo = Memo.Make (struct
-        module F = ListPlus.MakeHashedList (CilData.CilFundec)
-        module I = ListPlus.MakeHashedList (Instruction)
-        type t = Job.t * CilData.CilFundec.t list * Instruction.t list
-        let equal (j1, f1, i1) (j2, f2, i2) = j1 == j2 && F.equal f1 f2 && I.equal i1 i2
-        let hash (j, f, i) = Hashtbl.hash (j#jid_unique, F.hash f, I.hash i)
-    end) in
-    Memo.memo "ClosestToTargetsStrategy.get_distance_to_targets_impl" 
-        (fun (job, target_fundecs, line_targets) ->
-            let source = Job.get_instruction job in
-            let current_fundec = List.hd job#state.State.callstack in
-            let target_instrs = List.fold_left (fun lst f -> (Instruction.call_sites_in_caller job#file current_fundec f) @ lst) [] target_fundecs in
-            let all_targets = target_instrs @ line_targets in
-            if all_targets = [] then
-                max_distance
-            else
-                let context = Job.get_instruction_context job in
-                Distance.find_in_context (source, context, all_targets)
-        )
-
-let get_distance_to_targets job =
-    Profiler.global#call "ClosestToTargetsStrategy.get_distance_to_targets" begin fun () ->
-        let target_fundecs = BackOtterTargets.get_target_fundecs () in
-        let line_targets = LineTargets.get_line_targets job#file @ BackOtterTargetTracker.get_line_targets job#file in
-        let distance = get_distance_to_targets_impl (job, target_fundecs, line_targets) in
-        Output.debug_printf "Job %d has distance to target = %d@\n" job#node_id distance;
-        distance
-    end
-
-class ['self] t = object (self : 'self)
-
-    method add job = self
-
-    method remove job = self
-
-    method weight job =
-        let distance = get_distance_to_targets job in 
-        1. /. float_of_int (distance)
+module JobMap = struct
+    module M = Map.Make (struct type t = int let compare = Pervasives.compare end)
+    let empty = M.empty
+    let remove job jobs = M.remove job#node_id jobs
+    let add job x jobs = M.add job#node_id x jobs
+    let find job jobs = M.find job#node_id jobs
 end
 
+class ['self] t = object (self : 'self)
+    val distances = JobMap.empty
 
+    method add job =
+        (* if the targets change, the distance calculated and stored here may be off, but it saves from
+         * having to recalculate at every query *)
+        Profiler.global#call "ClosestToTargetsStrategy.add" begin fun () ->
+            let source = Job.get_instruction job in
+            let call_sites = Instruction.call_sites (Instruction.fundec_of source) in
+
+            (* TODO: maybe memoize the target calculation, if it turns out to be expensive *)
+            let target_fundecs = BackOtterTargets.get_target_fundecs () in
+            let target_instrs = List.filter begin fun call_site ->
+                List.exists (CilData.CilFundec.equal call_site.Instruction.fundec) target_fundecs
+            end call_sites in
+            let line_targets = LineTargets.get_line_targets job#file @ BackOtterTargetTracker.get_line_targets job#file in
+            let all_targets = target_instrs @ line_targets in
+
+            let distance =
+                if all_targets = [] then
+                    max_int
+                else
+                    let context = Job.get_instruction_context job in
+                    Distance.find_in_context (source, context, all_targets)
+            in
+            Output.debug_printf "Job %d has distance to target = %d@\n" job#node_id distance;
+            {< distances = JobMap.add job distance distances >}
+        end
+
+    method remove job =
+        {< distances = JobMap.remove job distances >}
+
+    method weight job =
+        1. /. float_of_int (JobMap.find job distances)
+end
 
