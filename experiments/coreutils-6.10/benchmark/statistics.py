bbf9f95317d9652e5558c5bbdb2ed8517a8c83d6
Kin-Keung Ma <kkma@cs.umd.edu>
2011-03-12 22:22:14 +0000

    Print the mean and stdev for each sample set

--- experiments/coreutils-6.10/benchmark/statistics.py
+++ experiments/coreutils-6.10/benchmark/statistics.py
@@ -2,7 +2,7 @@
 
 # Find the first occurence of "TargetReached" in Otter's output piped to timelines
 
-import sys, re, os, getopt
+import sys, re, os, math
 from collections import defaultdict
 
 # type key = Pure | Mix(strategy, ratio)
@@ -12,6 +12,17 @@ from collections import defaultdict
 
 # program: filename%_\d+\.log
 # seed: --random-seed=seed
+
+def mean(values):
+    if len(values) == 0:
+        raise Exception("Empty list")
+    return sum(values) / len(values)
+
+def stdev(values):
+    if len(values) == 0:
+        raise Exception("Empty list")
+    m = mean(values)
+    return math.sqrt(mean([(x-m)*(x-m) for x in values]))
 
 def loose_getopt(args, long_options):
     i = 0
@@ -30,7 +41,7 @@ command_re = re.compile(r"^Command: .*(runotter|runbackotter) (.*)")
 time_re = re.compile(r"^\s*\d+\.\d+\s*(\d+\.\d+).*TargetReached.*")
 program_re = re.compile(r"(.*)_\d+\.log")
 
-def main():
+def create_table():
     table = defaultdict(list)
     directory = sys.argv[1]
     filenames = os.listdir(directory)
@@ -53,6 +64,9 @@ def main():
                 break
     return table
 
-table = main()
-print table
+table = create_table()
+stat = dict()
+for key, values in table.items():
+    stat[key] = (mean(values), stdev(values))
+    print key, stat[key]
 
