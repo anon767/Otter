2fa52e6087c054e204d8922ee9ba8a6544db9fdc
Kin-Keung Ma <kkma@cs.umd.edu>
2011-03-13 00:46:33 +0000

    Generate tables to be put in latex. Untested.

--- experiments/coreutils-6.10/benchmark/statistics.py
+++ experiments/coreutils-6.10/benchmark/statistics.py
@@ -2,16 +2,8 @@
 
 # Find the first occurence of "TargetReached" in Otter's output piped to timelines
 
-import sys, re, os, math
+import sys, re, os, math, string
 from collections import defaultdict
-
-# type key = Pure | Mix(strategy, ratio)
-# Pure: runbackotter --bidirectional-search-ratio=-1
-# Mix(strategy, 0%): runotter --queue=strategy
-# Mix(strategy, ratio): runbackotter --forward-queue=strategy --bidirectional-search-ratio=ratio
-
-# program: filename%_\d+\.log
-# seed: --random-seed=seed
 
 def mean(values):
     if len(values) == 0:
@@ -41,33 +33,79 @@ command_re = re.compile(r"^Command: .*(runotter|runbackotter) (.*)")
 time_re = re.compile(r"^\s*\d+\.\d+\s*(\d+\.\d+).*TargetReached.*")
 program_re = re.compile(r"(.*)_\d+\.log")
 
-def create_table():
-    table = defaultdict(list)
-    directory = sys.argv[1]
-    filenames = os.listdir(directory)
-    for filename in filenames:
-        program = program_re.match(filename).group(1)
-        filename = os.path.join(directory, filename)
-        file = open(filename)
-        time = 100000.0  # Penalty
-        for line in file.readlines():
-            results = command_re.match(line)
-            if results:
-                cmd = results.group(1)
-                args = re.split(" |\t|\r|\n|=", results.group(2))
-                optlist = loose_getopt(args, ["queue=", "forward-queue=", "bidirectional-search-ratio="])
-                key = frozenset([cmd] + optlist)
-                continue
-            results = time_re.match(line)
-            if results:
-                time = float(results.group(1))
-                break
-        table[(program, key)].append(time)
-    return table
+table = defaultdict(list)
+programs = []
 
-table = create_table()
+directory = sys.argv[1]
+filenames = os.listdir(directory)
+for filename in filenames:
+    program = program_re.match(filename).group(1)
+    programs.append(program)
+    filename = os.path.join(directory, filename)
+    file = open(filename)
+    time = 100000.0  # Penalty
+    for line in file.readlines():
+        results = command_re.match(line)
+        if results:
+            cmd = results.group(1)
+            args = re.split(" |\t|\r|\n|=", results.group(2))
+            optlist = loose_getopt(args, ["queue=", "forward-queue=", "bidirectional-search-ratio="])
+            key = frozenset([cmd] + optlist)
+            continue
+        results = time_re.match(line)
+        if results:
+            time = float(results.group(1))
+            break
+    table[(program, key)].append(time)
+
 stat = dict()
 for key, values in table.items():
-    stat[key] = (len(values), mean(values), stdev(values))
-    print key, stat[key]
+    stat[key] = { "len":len(values), "mean":mean(values), "stdev":stdev(values) }
+
+
+# type key = Pure | Mix(strategy, ratio)
+# Pure: runbackotter --bidirectional-search-ratio=-1
+# Mix(strategy, 0%): runotter --queue=strategy
+# Mix(strategy, ratio): runbackotter --forward-queue=strategy --bidirectional-search-ratio=ratio
+
+# program: filename%_\d+\.log
+# seed: --random-seed=seed
+
+def format(entry):
+    return "\\meanstdev{%.2f}{%.2f}" % (entry["mean"], entry["stdev"])
+
+def getstat(opts):
+    global stat
+    key = frozenset(opts)
+    if key not in stat:
+        return {"mean":-1.0,"stdev":-1.0}
+    else:
+        return stat[key]
+
+print stat
+
+for program in programs:
+    output = [ program ]
+
+    # Pure BackOtter
+    output.append(format(getstat(["runbackotter",("bidirectional-search-ratio","-1")])))
+
+    for strategy in ["KLEE", "SAGE", "random-path", "depth-first"]:
+        # Pure forward
+        output.append(format(getstat(["runotter",("queue",strategy)])))
+        for ratio in [ ".25", ".5" ]:
+            output.append(format(getstat(["runbackotter",("forward-queue",strategy),("bidirectional-search-ratio",ratio)])))
+
+    # Naive distance to targets (TODO)
+    output.append(format(getstat(["runotter",("queue","naive-distance-to-targets")])))
+
+    # Simple ESD (TODO)
+    output.append(format(getstat(["runotter",("queue","simple-esd")])))
+
+    # join
+    result = string.join(output, " & ") + " \\\\"
+    print result
+
+
+
 
