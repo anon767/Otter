a9124848a535526d8a44064b39316525a97a42f2
Kin-Keung Ma <kkma@cs.umd.edu>
2011-03-19 19:56:41 +0000

    Generate tests one pile per random seed, and randomize the order of a pile during benchmarking.

--- experiments/coreutils-6.10/benchmark/setup_tests.py
+++ experiments/coreutils-6.10/benchmark/setup_tests.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-import sys, os, re, itertools, string
+import sys, os, re, itertools, string, random
 
 if len(sys.argv) < 5:
     print "Usage: ./setup_tests.py <output_dir> <experiment_name> <trunk_dir> <programs_list> <commands_list> [<commands_list2>...]"
@@ -10,8 +10,10 @@ exp_name = sys.argv[2].replace(" ", "_")
 exp_base = os.path.join(base, exp_name)
 
 trunk_dir = sys.argv[3]
-programs_in = sys.argv[4]
-options_ins = sys.argv[5:]
+min_seed = int(sys.argv[4])
+max_seed = int(sys.argv[5])
+programs_in = sys.argv[6]
+options_ins = sys.argv[7:]
 
 def add_trunk(line, trunk_dir):
     p = re.compile(r"@TRUNK@")
@@ -33,45 +35,62 @@ def mkdir_p(path, mode=0755):
     if not os.path.exists(path):
         os.makedirs(path, mode)
 
+def get_seed_option(seed):
+    return "--random-seed=%d" % seed
 
 mkdir_p(exp_base)
 config = os.path.join(exp_base, "config.log")
 f_config = open(config, "w")
-
-options_list = [""]
-for options_in in options_ins:
-    options = filter(lambda line: not_comment(line), [line.rstrip() for line in open(options_in).readlines()])
-    options_list = [string.join(x) for x in itertools.product(options_list, options)]
-
-for prog_opt in open(programs_in):
-    prog_opt = prog_opt.rstrip()
-    if not_comment(prog_opt):
-        print "Process %s" % prog_opt
-    else:
-        continue
-    prog_opt = add_trunk(prog_opt, trunk_dir)
-    prog_name, prog_opt = get_prog_name_opt(prog_opt)
-    options_id = 1
-
-    for options in options_list:
-        print "Process %s" % options
-        options = add_trunk(options, trunk_dir)
-        log_file = os.path.join(exp_base, "results", "%s_%d.log" % (prog_name, options_id))
-        test_sh = os.path.join(exp_base, "tests", "%s_%d.sh" % (prog_name, options_id))
-        mkdir_p(os.path.dirname(test_sh))
-
-        f_test_sh = open(test_sh, "w")
-        print >> f_test_sh,'mkdir -p "%s"' % os.path.dirname(log_file)
-        print >> f_test_sh,'echo Command: %s %s >> "%s"' % (options, prog_opt, log_file)
-        print >> f_test_sh,'%s %s 2>&1 | timelines >> "%s"' % (options, prog_opt, log_file)
-        print >> f_config,'%s_%d.sh: %s %s' % (prog_name, options_id, options, prog_opt)
-        f_test_sh.close()
-
-        options_id += 1
-
-f_config.close()
-
 at_sh = os.path.join(exp_base, "at.sh")
 f_at_sh = open(at_sh, "w")
-print >> f_at_sh,'find "%s" -type f | xargs -P 15 -n 1 sh' % (os.path.join(exp_base, "tests"))
 
+for seed in range(min_seed, max_seed+1):
+
+    seed_option = get_seed_option(seed)
+
+    options_list = [ "" ]
+    for options_in in options_ins:
+        options = filter(lambda line: not_comment(line), [line.rstrip() for line in open(options_in).readlines()])
+        options_list = [string.join(x) for x in itertools.product(options_list, options)]
+    options_list = [ x + " " + seed_option for x in options_list ]
+
+    test_sh_list = []
+    for prog_opt in open(programs_in):
+        prog_opt = prog_opt.rstrip()
+        if not_comment(prog_opt):
+            print "Process %s" % prog_opt
+        else:
+            continue
+        prog_opt = add_trunk(prog_opt, trunk_dir)
+        prog_name, prog_opt = get_prog_name_opt(prog_opt)
+        options_id = 1
+
+        for options in options_list:
+            print "Process %s" % options
+            options = add_trunk(options, trunk_dir)
+            log_file = os.path.join(exp_base, "results", str(seed), "%s_%d.log" % (prog_name, options_id))
+            test_sh = os.path.join(exp_base, "tests", str(seed), "%s_%d.sh" % (prog_name, options_id))
+            test_sh_list.append(test_sh)
+            mkdir_p(os.path.dirname(test_sh))
+
+            f_test_sh = open(test_sh, "w")
+            print >> f_test_sh,'mkdir -p "%s"' % os.path.dirname(log_file)
+            print >> f_test_sh,'echo Command: %s %s >> "%s"' % (options, prog_opt, log_file)
+            print >> f_test_sh,'%s %s 2>&1 | timelines >> "%s"' % (options, prog_opt, log_file)
+            print >> f_config,'%s_%d.sh: %s %s' % (prog_name, options_id, options, prog_opt)
+            f_test_sh.close()
+
+            options_id += 1
+
+    test_order = os.path.join(exp_base, "tests", str(seed), "ordering.txt")
+    f_test_order = open(test_order, "w")
+    # Shuffle the tests 7 times
+    for i in range(0,7):
+        random.shuffle(test_sh_list)
+    for test_sh in test_sh_list:
+        print >> f_test_order, test_sh
+    f_test_order.close()
+    print >> f_at_sh,'cat "%s" | xargs -P 15 -n 1 sh' % test_order
+
+f_config.close()
+f_at_sh.close()
