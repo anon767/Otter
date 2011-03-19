d19039765df7f0da2258e507d357e14f558487ee
Kin-Keung Ma <kkma@cs.umd.edu>
2011-03-19 18:00:25 +0000

    Looks like test*.cov are always generated, but no test*.ktest.

--- experiments/coreutils-6.10/benchmark/ktest-monitor.sh
+++ experiments/coreutils-6.10/benchmark/ktest-monitor.sh
@@ -12,7 +12,7 @@ testno=1
 while true
 do 
     sleep 0.1
-    testfile=$(printf "test%06d.ktest" $testno)
+    testfile=$(printf "test%06d.cov" $testno)
     if [ -f "$dir/$testfile" ]; then
         time_elapsed=$(echo $(gettimeofday) - $start_time|bc)
         echo "$testfile $time_elapsed"
