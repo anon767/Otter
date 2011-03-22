4a1952bf0a35fa1839f83a90dac58d7f89b38485
Kin-Keung Ma <kkma@cs.umd.edu>
2011-03-22 06:11:33 +0000

    This is new more similar to pro_backotter_2, while look more realistic. BackOtter wins.

--- experiments/coreutils-6.10/benchmark/synthetic/pro_backotter_1.c
+++ experiments/coreutils-6.10/benchmark/synthetic/pro_backotter_1.c
@@ -1,27 +1,32 @@
-// Otter (~12 secs)
+// Otter:
+// <path>/otter.pl --doexecute --max-abandoned=1 --queue=closest-to-targets --random-seed=0 pro_backotter_1.c --printLittle
 // <path>/otter.pl --doexecute --max-abandoned=1 --queue=random-path --random-seed=0 pro_backotter_1.c --printLittle
+// <path>/otter.pl --doexecute --max-abandoned=1 --queue=KLEE --random-seed=0 pro_backotter_1.c --printLittle
 //
-// BackOtter (~2 secs)
+// BackOtter:
 // <path>/otter.pl --dobackotter --max-abandoned=1 --bidirectional-search-ratio=-1 --backward-queue=random-path --random-seed=0 pro_backotter_1.c --backward-function-rank=closest-to-entry --printLittle
 //
+#define MAX 8
 void __FAILURE(void) {}
 
 void f(int n) { 
-    int j, c = 0;
-    for(j=0;j<9;j++) {
-        int m;
-        __SYMBOLIC(&m);
+    int i, c = 0;
+    for(i=0;i<MAX;i++) {
+        int m; __SYMBOLIC(&m);
+        c *= 2;
         if (m) c++;
     }
-    if (c == 0 && n == 5/* rarely satisfied condition */) __FAILURE(); 
+    /* rarely satisfied condition */
+    if (c == 226 && n == 128) __FAILURE(); 
 }
 
 void main() {
-    int n, i; 
-    __SYMBOLIC(&n);
-    /* Lots of branches induced by i */
-    for (i=0;i<50;i++)
-        if(n==i) break;
-    f(n);
+    int i, c = 0;
+    for(i=0;i<MAX;i++) {
+        int m; __SYMBOLIC(&m);
+        c *= 2;
+        if (m) c++;
+    }
+    f(c);
 }
 
