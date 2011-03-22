d25d1d084b965b051c7d23a60ab49596f4356064
Kin-Keung Ma <kkma@cs.umd.edu>
2011-03-22 22:54:24 +0000

    Made this example look the same as in the paper.
    Also, include macros so that this example can be run on real KLEE.

--- experiments/coreutils-6.10/benchmark/synthetic/pro_backotter_1.c
+++ experiments/coreutils-6.10/benchmark/synthetic/pro_backotter_1.c
@@ -6,24 +6,39 @@
 // BackOtter:
 // <path>/otter.pl --dobackotter --max-abandoned=1 --bidirectional-search-ratio=-1 --backward-queue=random-path --random-seed=0 pro_backotter_1.c --backward-function-rank=closest-to-entry --printLittle
 //
-#define MAX 8
+#define MAX   5
+#define MAX_F 12
 void __FAILURE(void) {}
 
-void f(int n) { 
-    int i, c = 0, input[MAX];
-    __SYMBOLIC(&input);
-    for(i=0;i<MAX;i++) {
-        if (input[i]) c++;
+#ifdef KLEE
+  #include <assert.h>
+  #define symbolic(V,S,N)   klee_make_symbolic(V,S,N)
+  #define failure()         assert(0)
+#else
+  #ifdef CIL
+    #define symbolic(V,S,N)   __SYMBOLIC(V)
+    #define failure()         __FAILURE()
+  #else
+    #warning "Either run with CIL or KLEE"
+  #endif
+#endif
+
+void f(int c) { 
+    int i, d = 0;
+    int m[MAX_F];
+    symbolic(&m,sizeof(m),"m");
+    for(i=0;i<MAX_F;i++) {
+        if (m[i]) d++;
     }
-    /* rarely satisfied condition */
-    if (c == 3 && n == 4) __FAILURE(); 
+    if (c == 0 && d == MAX_F) failure(); 
 }
 
 void main() {
-    int i, c = 0, input[MAX];
-    __SYMBOLIC(&input);
+    int i, c = 0;
+    int n[MAX];
+    symbolic(&n,sizeof(n),"n");
     for(i=0;i<MAX;i++) {
-        if (input[i]) c++;
+        if (n[i]) c++;
     }
     f(c);
 }
