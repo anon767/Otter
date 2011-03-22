e616962d9a5e8b6410b9a00d2e0aae43b90fa867
Kin-Keung Ma <kkma@cs.umd.edu>
2011-03-22 23:28:25 +0000

    Modified this example in the same way as r11917. Plus, increase the loop bound from 100 to 1000. This makes BackOtter faster than real KLEE.

--- experiments/coreutils-6.10/benchmark/synthetic/pro_backotter_3.c
+++ experiments/coreutils-6.10/benchmark/synthetic/pro_backotter_3.c
@@ -1,18 +1,31 @@
 void __FAILURE(void) {}
 
+#ifdef KLEE
+  #include <assert.h>
+  #define symbolic(V,S,N)   klee_make_symbolic(V,S,N)
+  #define failure()         assert(0)
+#else
+  #ifdef CIL
+    #define symbolic(V,S,N)   __SYMBOLIC(V)
+    #define failure()         __FAILURE()
+  #else
+    #warning "Either run with CIL or KLEE"
+  #endif
+#endif
+
 void f(int m) {
     int g = 0;
 
     while(1) {
-        if(g > 10 && m > 10) __FAILURE();
-        else { g++; g++; }
+        if(g > 10 && m > 10) failure();
+        else g++;
     }
 }
 
 void main() {
     int n, i;
-    __SYMBOLIC(&n);
+    symbolic(&n, sizeof(n), "n");
 
-    for (i=0;i<100;i++) 
+    for (i=0;i<1000;i++) 
         if (n == i) f(i);
 }
