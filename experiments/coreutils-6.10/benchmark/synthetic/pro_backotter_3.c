b3f5b2ea2d2351ea6d8b6ef28045f7d7d87d71cd
Kin-Keung Ma <kkma@cs.umd.edu>
2011-03-24 18:00:02 +0000

    Updated to match Fig 2 in the paper (r11972).

--- experiments/coreutils-6.10/benchmark/synthetic/pro_backotter_3.c
+++ experiments/coreutils-6.10/benchmark/synthetic/pro_backotter_3.c
@@ -13,11 +13,11 @@ void __FAILURE(void) {}
   #endif
 #endif
 
-void f(int m) {
+void f(int n) {
     int g = 0;
 
     while(1) {
-        if(g > 10 && m > 10) failure();
+        if(g > 1000 && n == 0) failure();
         else g++;
     }
 }
@@ -26,6 +26,13 @@ void main() {
     int n, i;
     symbolic(&n, sizeof(n), "n");
 
-    for (i=0;i<1000;i++) 
-        if (n == i) f(i);
+    for (i=1;i<1000;i++)
+        if (n == i) break;
+    switch (i) {
+        case 1: 
+        case 2: 
+        case 3: 
+        case 4: f(1); break;
+        default: f(0);
+    }
 }
