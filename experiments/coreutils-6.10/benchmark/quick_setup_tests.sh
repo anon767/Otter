a57864569b6d82b2cf1576feda85fcb02753b43f
Kin-Keung Ma <kkma@cs.umd.edu>
2011-03-28 19:06:38 +0000

    update

--- experiments/coreutils-6.10/benchmark/quick_setup_tests.sh
+++ experiments/coreutils-6.10/benchmark/quick_setup_tests.sh
@@ -3,5 +3,6 @@ trunk="$PWD/../../../"
 min_seed=$2 
 max_seed=$3 
 output_dir=$4
-svn_version=$(svnversion "$trunk" | sed 's/:/_/')
-./setup_tests.py "$output_dir" "$svn_version $min_seed $max_seed $1" "$trunk" $min_seed $max_seed programs.in options.in options-common.in
+trunk_version=$(svnversion "$trunk" | sed 's/:/_/')
+otter_version=$(svnversion "$trunk/otter" | sed 's/:/_/')
+./setup_tests.py "$output_dir" "otter $otter_version trunk $trunk_version $min_seed $max_seed $1" "$trunk" $min_seed $max_seed programs.in options.in options-common.in
