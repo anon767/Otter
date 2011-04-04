ffad96d97163fadc0fa8b90c6646673e3d78cf68
Kin-Keung Ma <kkma@cs.umd.edu>
2011-04-04 17:02:52 +0000

    Removed after r12241

--- experiments/coreutils-6.10/benchmark/quick_setup_tests.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-trunk="$PWD/../../../"
-min_seed=$2 
-max_seed=$3 
-output_dir=$4
-trunk_version=$(svnversion "$trunk" | sed 's/:/_/')
-otter_version=$(svnversion "$trunk/otter" | sed 's/:/_/')
-./setup_tests.py "$output_dir" "otter $otter_version trunk $trunk_version $min_seed $max_seed $1" "$trunk" $min_seed $max_seed programs.in options.in options-common.in
