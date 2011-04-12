1c945618d409e634579e867a18aa4d52830d8498
Elnatan Reisner <elnatan@cs.umd.edu>
2011-04-12 00:44:50 +0000

    Whitespace

--- newlib-1.19.0/otter/syscalls/stdio/_rename_r.c
+++ newlib-1.19.0/otter/syscalls/stdio/_rename_r.c
@@ -22,7 +22,7 @@
 int _rename_r(struct _reent *ignored, const char *old_path, const char *new_path) {
 	struct __otter_fs_dnode *old_dir, *new_dir;
 	char *old_filename, *new_filename;
-	struct __otter_fs_inode	*old_inode;
+	struct __otter_fs_inode *old_inode;
 
 	// Find the old (target) directory and inode
 	old_dir = find_filename_and_dnode(old_path, &old_filename);
@@ -37,12 +37,12 @@ int _rename_r(struct _reent *ignored, const char *old_path, const char *new_path
 	// Atomically rename the file
 	__otter_multi_begin_atomic();
 
-  /* Unlink the new file, if it exists. If it doesn't exist, this will return an error, but we can ignore it. */
-  __otter_fs_unlink_in_dir(new_filename, new_dir);
-  /* Link the new filename to old_inode */
-  __otter_fs_link_file(new_filename, old_inode, new_dir);
-  /* Unlink the old file */
-  __otter_fs_unlink_in_dir(old_filename, old_dir);
+	/* Unlink the new file, if it exists. If it doesn't exist, this will return an error, but we can ignore it. */
+	__otter_fs_unlink_in_dir(new_filename, new_dir);
+	/* Link the new filename to old_inode */
+	__otter_fs_link_file(new_filename, old_inode, new_dir);
+	/* Unlink the old file */
+	__otter_fs_unlink_in_dir(old_filename, old_dir);
 
 	__otter_multi_end_atomic();
 
