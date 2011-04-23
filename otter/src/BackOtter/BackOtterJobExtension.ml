9e001318c9da75f7b020691f4ee9c4fce09d2f09
Kin-Keung Ma <kkma@cs.umd.edu>
2011-04-23 21:41:45 +0000

    Minor edit

--- otter/src/BackOtter/BackOtterJobExtension.ml
+++ otter/src/BackOtter/BackOtterJobExtension.ml
@@ -1,7 +1,12 @@
 class t :
     object ('self)
-        method bounding_paths : OtterCore.Decision.t list list option
-        method with_bounding_paths : OtterCore.Decision.t list list option -> 'self
+        (** Bounding paths
+         *  None: this job is NOT a bounded job
+         *  Some []: this is a bounded job, and has fallen out of bound
+         *  Some paths: this is a bounded job, and is still bounded
+         *)
+        method bounding_paths : OtterCore.DecisionPath.t list option
+        method with_bounding_paths : OtterCore.DecisionPath.t list option -> 'self
         method become : 'self -> unit
     end
 =
