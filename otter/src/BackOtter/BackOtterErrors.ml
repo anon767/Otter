3fbcde44f5c1a2111ab89589e8c95de27029a6ec
Kin-Keung Ma <kkma@cs.umd.edu>
2011-05-06 17:56:32 +0000

    Further avoid line breakings in reason tags in an error message (similar to r12666)

--- otter/src/BackOtter/BackOtterErrors.ml
+++ otter/src/BackOtter/BackOtterErrors.ml
@@ -12,19 +12,19 @@ type t = [
 
 
 let option_printer printer ff = function
-    | Some x -> Format.fprintf ff "Some (@[%a@]@,)" printer x
+    | Some x -> Format.fprintf ff "Some (@[<h>%a@]@,)" printer x
     | None -> Format.fprintf ff "None"
 
 
 let rec printer ff (error : t) = match error with
     | `SummaryReturn return_opt ->
-        Format.fprintf ff "`SummaryReturn(@[%a@])" (option_printer BytesPrinter.bytes) return_opt
+        Format.fprintf ff "`SummaryReturn(@[<h>%a@])" (option_printer BytesPrinter.bytes) return_opt
 
     | `SummaryExit exit_opt ->
-        Format.fprintf ff "`SummaryExit(@[%a@])" (option_printer BytesPrinter.bytes) exit_opt
+        Format.fprintf ff "`SummaryExit(@[<h>%a@])" (option_printer BytesPrinter.bytes) exit_opt
 
     | `SummaryAbandoned reason ->
-        Format.fprintf ff "`SummaryAbandoned(@[%a@])" printer reason
+        Format.fprintf ff "`SummaryAbandoned(@[<h>%a@])" printer reason
 
     | #Errors.t as x -> Errors.printer ff x
 
