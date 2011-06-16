0a907952f5e9de24a8e8794f76ffbb744adc8a58
Kin-Keung Ma <kkma@cs.umd.edu>
2011-06-16 20:10:59 +0000

    Added a stub for __otter_multi_get_parent_pid

--- newlib-1.19.0/otter/syscalls/otter/multiotter_builtins_stub.c
+++ newlib-1.19.0/otter/syscalls/otter/multiotter_builtins_stub.c
@@ -5,6 +5,7 @@
 #include <unistd.h> // For size_t and pid_t
 
 #define __otter_PID 1
+#define __otter_PPID 2
 
 void *__otter_multi_gmalloc(size_t size) {
     return malloc(size);
@@ -23,9 +24,14 @@ pid_t __otter_multi_get_pid(void) {
     return __otter_PID;
 }
 
+// tac calls getppid just for some random seed, 
+// therefore it's possible for some non-concurrent programs to invoke concurrent functions.
+pid_t __otter_multi_get_parent_pid(pid_t pid) {
+    return  __otter_PPID;
+}
+
 // Let these be undefined
 // void __otter_multi_set_parent_pid(pid_t pid);
-// pid_t __otter_multi_get_parent_pid(pid_t pid); 
 
 // Defined in stdlib/calloc.c and realloc.c
 // void *__otter_multi_gcalloc(size_t count, size_t size); 
