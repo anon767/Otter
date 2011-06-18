d70f92d0883338c1bed9320a4925759cb523dba3
Khoo Yit Phang <khooyp@cs.umd.edu>
2011-06-18 16:35:57 +0000

    Odd bug that occurs if a function is used before declared and the return type is mismatched.
    This is a type error that would normally be caught by compilers. CIL's Check module can also catch this (--strictchecking), so I'm inclined to fix this bug by automatically enforcing the check.

---
+++ otter/test/TestOtterIntegration/OtterCore/WeirdBehavior/sign_extend_return_value.c
@@ -0,0 +1,22 @@
+/* This weird bug occurs when a function is used before it's defined and without declaring its prototype, and the
+ * return value type does not match the destination type.
+ *
+ * Normally, if the prototype is correctly declared and the return types do not match, CIL will insert an explicit cast
+ * which Otter handles correctly.
+ *
+ * When the prototype isn't declared, the return type is implicitly int. If the actual return type is not an int,
+ * compilers such as GCC or Clang will report an error. However, CIL silently allows such code, but does not insert
+ * an explicit cast to covert the return value to an int, and will generate bad Call's with mismatched types.
+ */
+#pragma no_other_abandoned
+
+int main(void) {
+    int x = foo(); /* no prototype declared so the return type is mismatched */
+    __ASSERT(x > 0);
+    return 0;
+}
+
+unsigned char foo(void) {
+    return 0xff;
+}
+
