53c248954b69ca8f1f8d1a6ff4c5b32127667786
Kin-Keung Ma <kkma@cs.umd.edu>
2011-07-17 18:49:28 +0000

    More utilities

--- otter/src/OcamlUtilities/Module.ml
+++ otter/src/OcamlUtilities/Module.ml
@@ -18,3 +18,13 @@ module CombineHashedTypes3 (T1 : Hashtbl.HashedType) (T2 : Hashtbl.HashedType) (
     let hash (t1, t2, t3) = Hashtbl.hash (T1.hash t1, T2.hash t2, T3.hash t3)
     let equal (t1, t2, t3) (t1', t2', t3') = T1.equal t1 t1' && T2.equal t2 t2' && T3.equal t3 t3'
 end
+
+module CombineOrderedTypes (T1 : Map.OrderedType) (T2 : Map.OrderedType) : Map.OrderedType with type t = T1.t * T2.t = struct
+    type t = T1.t * T2.t
+    let compare (t1, t2) (t1', t2') = match T1.compare t1 t1' with 0 -> T2.compare t2 t2' | i -> i
+end
+
+module CombineOrderedTypes3 (T1 : Map.OrderedType) (T2 : Map.OrderedType) (T3 : Map.OrderedType) : Map.OrderedType with type t = T1.t * T2.t * T3.t = struct
+    type t = T1.t * T2.t * T3.t
+    let compare (t1, t2, t3) (t1', t2', t3') = match T1.compare t1 t1' with 0 -> (match T2.compare t2 t2' with 0 -> T3.compare t3 t3' | i -> i) | i -> i
+end
