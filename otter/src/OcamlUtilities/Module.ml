86149dd07e595855ace26688cef471cc83bdcde5
Kin-Keung Ma <kkma@cs.umd.edu>
2011-07-21 22:49:51 +0000

    added module Tuple that contains utilities for tuple manipulations

--- otter/src/OcamlUtilities/Module.ml
+++ otter/src/OcamlUtilities/Module.ml
@@ -9,22 +9,22 @@ end
 
 module CombineHashedTypes (T1 : Hashtbl.HashedType) (T2 : Hashtbl.HashedType) : Hashtbl.HashedType with type t = T1.t * T2.t = struct
     type t = T1.t * T2.t
-    let hash (t1, t2) = Hashtbl.hash (T1.hash t1, T2.hash t2)
-    let equal (t1, t2) (t1', t2') = T1.equal t1 t1' && T2.equal t2 t2'
+    let hash = Tuple.combine_hashes T1.hash T2.hash
+    let equal = Tuple.combine_equals T1.equal T2.equal
 end
 
 module CombineHashedTypes3 (T1 : Hashtbl.HashedType) (T2 : Hashtbl.HashedType) (T3 : Hashtbl.HashedType) : Hashtbl.HashedType with type t = T1.t * T2.t * T3.t = struct
     type t = T1.t * T2.t * T3.t
-    let hash (t1, t2, t3) = Hashtbl.hash (T1.hash t1, T2.hash t2, T3.hash t3)
-    let equal (t1, t2, t3) (t1', t2', t3') = T1.equal t1 t1' && T2.equal t2 t2' && T3.equal t3 t3'
+    let hash = Tuple.combine_hashes3 T1.hash T2.hash T3.hash
+    let equal = Tuple.combine_equals3 T1.equal T2.equal T3.equal
 end
 
 module CombineOrderedTypes (T1 : Map.OrderedType) (T2 : Map.OrderedType) : Map.OrderedType with type t = T1.t * T2.t = struct
     type t = T1.t * T2.t
-    let compare (t1, t2) (t1', t2') = match T1.compare t1 t1' with 0 -> T2.compare t2 t2' | i -> i
+    let compare = Tuple.combine_compares T1.compare T2.compare
 end
 
 module CombineOrderedTypes3 (T1 : Map.OrderedType) (T2 : Map.OrderedType) (T3 : Map.OrderedType) : Map.OrderedType with type t = T1.t * T2.t * T3.t = struct
     type t = T1.t * T2.t * T3.t
-    let compare (t1, t2, t3) (t1', t2', t3') = match T1.compare t1 t1' with 0 -> (match T2.compare t2 t2' with 0 -> T3.compare t3 t3' | i -> i) | i -> i
+    let compare = Tuple.combine_compares3 T1.compare T2.compare T3.compare
 end
