9c8fc97ace058c8c36106b6af9fd489bab09f33b
Khoo Yit Phang <khooyp@cs.umd.edu>
2011-08-01 07:18:39 +0000

    Document the reason for this segfault: it's a stack overflow due to a deeply-nested array expression.

--- otter/test/TestOtterIntegration/OtterCore/Bugs/array_read_segfault.c
+++ otter/test/TestOtterIntegration/OtterCore/Bugs/array_read_segfault.c
@@ -1,3 +1,9 @@
+/*  This test leads to stack overflows in many places in STP, because the memset(...) below creates a deeply-nested
+    STP array expression of the form: "(...(((a WITH [0] := 0) WITH [1] := 0) WITH [2] := 0) ...) WITH [40000] := 0".
+
+    There is no easy fix for this problem, other than increasing the stack limit.
+*/
+
 int const MAX = 10000;  /* smaller size doesn't fail */
 void main() {
     int a[MAX];
